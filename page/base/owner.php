0f268a978596014bb9af97771d344e8fdc7e6a81
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-10-28 09:52:39 +0530

    elfinder multiple css and js to one new version in admin part

--- page/base/owner.php
+++ page/base/owner.php
@@ -31,86 +31,9 @@ class page_base_owner extends Page {
 	}
 
 	function render(){
-		$this->api->template->appendHTML('js_include',
-				'
-					<link rel="stylesheet" href="elfinder/css/common.css"      type="text/css">
-					<link rel="stylesheet" href="elfinder/css/dialog.css"      type="text/css">
-					<link rel="stylesheet" href="elfinder/css/toolbar.css"     type="text/css">
-					<link rel="stylesheet" href="elfinder/css/navbar.css"      type="text/css">
-					<link rel="stylesheet" href="elfinder/css/statusbar.css"   type="text/css">
-					<link rel="stylesheet" href="elfinder/css/contextmenu.css" type="text/css">
-					<link rel="stylesheet" href="elfinder/css/cwd.css"         type="text/css">
-					<link rel="stylesheet" href="elfinder/css/quicklook.css"   type="text/css">
-					<link rel="stylesheet" href="elfinder/css/commands.css"    type="text/css">
-
-					<link rel="stylesheet" href="elfinder/css/fonts.css"       type="text/css">
-					<link rel="stylesheet" href="elfinder/css/theme.css"       type="text/css">
-
-					<!-- elfinder core -->
-					<script src="elfinder/js/elFinder.js"></script>
-					<script src="elfinder/js/elFinder.version.js"></script>
-					<script src="elfinder/js/jquery.elfinder.js"></script>
-					<script src="elfinder/js/elFinder.resources.js"></script>
-					<script src="elfinder/js/elFinder.options.js"></script>
-					<script src="elfinder/js/elFinder.history.js"></script>
-					<script src="elfinder/js/elFinder.command.js"></script>
-
-					<!-- elfinder ui -->
-					<script src="elfinder/js/ui/overlay.js"></script>
-					<script src="elfinder/js/ui/workzone.js"></script>
-					<script src="elfinder/js/ui/navbar.js"></script>
-					<script src="elfinder/js/ui/dialog.js"></script>
-					<script src="elfinder/js/ui/tree.js"></script>
-					<script src="elfinder/js/ui/cwd.js"></script>
-					<script src="elfinder/js/ui/toolbar.js"></script>
-					<script src="elfinder/js/ui/button.js"></script>
-					<script src="elfinder/js/ui/uploadButton.js"></script>
-					<script src="elfinder/js/ui/viewbutton.js"></script>
-					<script src="elfinder/js/ui/searchbutton.js"></script>
-					<script src="elfinder/js/ui/sortbutton.js"></script>
-					<script src="elfinder/js/ui/panel.js"></script>
-					<script src="elfinder/js/ui/contextmenu.js"></script>
-					<script src="elfinder/js/ui/path.js"></script>
-					<script src="elfinder/js/ui/stat.js"></script>
-					<script src="elfinder/js/ui/places.js"></script>
-
-					<!-- elfinder commands -->
-					<script src="elfinder/js/commands/back.js"></script>
-					<script src="elfinder/js/commands/forward.js"></script>
-					<script src="elfinder/js/commands/reload.js"></script>
-					<script src="elfinder/js/commands/up.js"></script>
-					<script src="elfinder/js/commands/home.js"></script>
-					<script src="elfinder/js/commands/copy.js"></script>
-					<script src="elfinder/js/commands/cut.js"></script>
-					<script src="elfinder/js/commands/paste.js"></script>
-					<script src="elfinder/js/commands/open.js"></script>
-					<script src="elfinder/js/commands/rm.js"></script>
-					<script src="elfinder/js/commands/info.js"></script>
-					<script src="elfinder/js/commands/duplicate.js"></script>
-					<script src="elfinder/js/commands/rename.js"></script>
-					<script src="elfinder/js/commands/help.js"></script>
-					<script src="elfinder/js/commands/getfile.js"></script>
-					<script src="elfinder/js/commands/mkdir.js"></script>
-					<script src="elfinder/js/commands/mkfile.js"></script>
-					<script src="elfinder/js/commands/upload.js"></script>
-					<script src="elfinder/js/commands/download.js"></script>
-					<script src="elfinder/js/commands/edit.js"></script>
-					<script src="elfinder/js/commands/quicklook.js"></script>
-					<script src="elfinder/js/commands/quicklook.plugins.js"></script>
-					<script src="elfinder/js/commands/extract.js"></script>
-					<script src="elfinder/js/commands/archive.js"></script>
-					<script src="elfinder/js/commands/search.js"></script>
-					<script src="elfinder/js/commands/view.js"></script>
-					<script src="elfinder/js/commands/resize.js"></script>
-					<script src="elfinder/js/commands/sort.js"></script>	
-					<script src="elfinder/js/commands/netmount.js"></script>
-
-					<script src="elfinder/js/jquery.dialogelfinder.js"></script>
-
-					<!-- elfinder 1.x connector API support -->
-					<script src="elfinder/js/proxy/elFinderSupportVer1.js"></script>
-					'
-				."\n");
+		$this->api->template->appendHTML('js_include','<link type="text/css" href="elfinder/css/elfinder.min.css" rel="stylesheet" />'."\n");
+			$this->api->template->appendHTML('js_include','<link type="text/css" href="elfinder/css/theme.css" rel="stylesheet" />'."\n");
+			$this->api->template->appendHTML('js_include','<script src="elfinder/js/elfinder.min.js"></script>'."\n");
 		parent::render();
 	}
 }
