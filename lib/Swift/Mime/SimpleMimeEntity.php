82ccc5592afd11448b5c9ccf4134f4d48fcc0ea1
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-10-30 16:57:07 +0530

    Updated: Swift mailer version

--- lib/Swift/Mime/SimpleMimeEntity.php
+++ lib/Swift/Mime/SimpleMimeEntity.php
@@ -263,7 +263,7 @@ class Swift_Mime_SimpleMimeEntity implements Swift_Mime_MimeEntity
     /**
      * Get all children added to this entity.
      *
-     * @return Swift_Mime_MimeEntity[]
+     * @return array of Swift_Mime_Entity
      */
     public function getChildren()
     {
@@ -273,8 +273,8 @@ class Swift_Mime_SimpleMimeEntity implements Swift_Mime_MimeEntity
     /**
      * Set all children of this entity.
      *
-     * @param Swift_Mime_MimeEntity[] $children
-     * @param int                     $compoundLevel For internal use only
+     * @param array   $children      Swift_Mime_Entity instances
+     * @param int     $compoundLevel For internal use only
      *
      * @return Swift_Mime_SimpleMimeEntity
      */
