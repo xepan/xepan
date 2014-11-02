b34048879d4f4f884e14e589875aad3e83da10cd
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-02 14:27:56 +0530

    install.sql updated

--- install.sql
+++ install.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 26, 2014 at 01:49 PM
--- Server version: 5.5.38
--- PHP Version: 5.3.10-1ubuntu3.13
+-- Generation Time: Nov 02, 2014 at 02:27 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.4
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `xepanai`
+-- Database: `xepan-marketing`
 --
 
 -- --------------------------------------------------------
@@ -100,13 +100,22 @@ CREATE TABLE IF NOT EXISTS `epan` (
   `email_password` varchar(255) DEFAULT NULL,
   `email_reply_to` varchar(255) DEFAULT NULL,
   `email_reply_to_name` varchar(255) DEFAULT NULL,
-  `email_from` varchar(255) DEFAULT NULL,
-  `email_from_name` varchar(255) DEFAULT NULL,
   `is_frontent_regiatrstion_allowed` tinyint(1) DEFAULT NULL,
   `user_activation` varchar(255) DEFAULT NULL,
-  `email_threshold` varchar(255) DEFAULT NULL,
+  `email_threshold` int(11) DEFAULT NULL,
   `user_registration_email_subject` varchar(255) DEFAULT NULL,
   `user_registration_email_message_body` text,
+  `email_transport` varchar(255) DEFAULT NULL,
+  `encryption` varchar(255) DEFAULT NULL,
+  `from_email` varchar(255) DEFAULT NULL,
+  `from_name` varchar(255) DEFAULT NULL,
+  `sender_email` varchar(255) DEFAULT NULL,
+  `sender_name` varchar(255) DEFAULT NULL,
+  `return_path` varchar(255) DEFAULT NULL,
+  `smtp_auto_reconnect` int(11) DEFAULT NULL,
+  `emails_in_BCC` int(11) DEFAULT NULL,
+  `last_emailed_at` datetime DEFAULT NULL,
+  `email_sent_in_this_minute` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_epan_staff1` (`staff_id`),
   KEY `fk_epan_epan_categories1` (`category_id`),
@@ -117,8 +126,8 @@ CREATE TABLE IF NOT EXISTS `epan` (
 -- Dumping data for table `epan`
 --
 
-INSERT INTO `epan` (`id`, `name`, `staff_id`, `branch_id`, `password`, `fund_alloted`, `created_at`, `category_id`, `company_name`, `contact_person_name`, `mobile_no`, `address`, `city`, `state`, `country`, `email_id`, `keywords`, `description`, `website`, `is_active`, `is_approved`, `last_email_sent`, `allowed_aliases`, `parked_domain`, `email_host`, `email_port`, `email_username`, `email_password`, `email_reply_to`, `email_reply_to_name`, `email_from`, `email_from_name`, `is_frontent_regiatrstion_allowed`, `user_activation`, `email_threshold`, `user_registration_email_subject`, `user_registration_email_message_body`) VALUES
-(1, 'web', 1, 1, 'admin', '5000000', '2014-01-26', 1, 'Xavoc Technocrats Pvt. Ltd.', 'Xavoc Admin', '+91 8875191258', '18/436, Gayatri marg, Kanji Ka hata, Udaipur, Rajasthan , India', 'Udaipur', 'Rajasthan', 'India', '', 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', 'World''s best and easiest cms :)', '', 1, 1, NULL, 1, NULL, '', '', '', '', '', '', '', '', 1, 'self_activated', '200', NULL, NULL);
+INSERT INTO `epan` (`id`, `name`, `staff_id`, `branch_id`, `password`, `fund_alloted`, `created_at`, `category_id`, `company_name`, `contact_person_name`, `mobile_no`, `address`, `city`, `state`, `country`, `email_id`, `keywords`, `description`, `website`, `is_active`, `is_approved`, `last_email_sent`, `allowed_aliases`, `parked_domain`, `email_host`, `email_port`, `email_username`, `email_password`, `email_reply_to`, `email_reply_to_name`, `is_frontent_regiatrstion_allowed`, `user_activation`, `email_threshold`, `user_registration_email_subject`, `user_registration_email_message_body`, `email_transport`, `encryption`, `from_email`, `from_name`, `sender_email`, `sender_name`, `return_path`, `smtp_auto_reconnect`, `emails_in_BCC`, `last_emailed_at`, `email_sent_in_this_minute`) VALUES
+(1, 'web', 1, 1, 'admin', '5000000', '2014-01-26', 1, 'Xavoc Technocrats Pvt. Ltd.', 'Xavoc Admin', '+91 8875191258', '18/436, Gayatri marg, Kanji Ka hata, Udaipur, Rajasthan , India', 'Udaipur', 'Rajasthan', 'India', '', 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', 'World''s best and easiest cms :)', 'http://www.xavoc.com', 1, 1, NULL, 1, NULL, '', '', '', '', '', '', 1, 'self_activated', 200, NULL, NULL, 'SendmailTransport', 'ssl', '', '', '', '', '', 0, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -196,7 +205,17 @@ CREATE TABLE IF NOT EXISTS `epan_components_marketplace` (
 --
 
 INSERT INTO `epan_components_marketplace` (`id`, `name`, `allowed_children`, `specific_to`, `namespace`, `type`, `is_system`, `description`, `default_enabled`, `has_toolbar_tools`, `has_owner_modules`, `has_plugins`, `has_live_edit_app_page`, `git_path`, `initialize_and_clone_from_git`, `category`) VALUES
-(51, 'Basic Web Elements And Plugins', '0', '0', 'baseElements', 'element', 1, '0', 1, 1, 0, NULL, 0, '', 0, NULL);
+(51, 'Basic Web Elements And Plugins', '0', '0', 'baseElements', 'element', 1, '0', 1, 1, 0, NULL, 0, '', 0, NULL),
+(67, 'Developer Zone', '0', '0', 'developerZone', 'application', 0, '0', 1, 0, 0, NULL, 0, '0', 0, NULL),
+(71, 'Extended Element', '0', '0', 'ExtendedElement', 'module', 0, '0', 1, 1, 0, NULL, 0, '0', 0, NULL),
+(69, 'Enquiry And Subscriptions', '0', '0', 'xEnquiryNSubscription', 'application', 0, '0', 1, 1, 0, NULL, 0, '0', 0, NULL),
+(70, 'xAtrificial Intelligence', '0', '0', 'xAi', 'application', 0, '0', 1, 1, 1, NULL, 0, 'https://github.com/xepan/xAi.git', 1, NULL),
+(72, 'Slide Shows', '0', '0', 'slideShows', 'application', 0, '0', 1, 1, 0, NULL, 0, '0', 1, NULL),
+(73, 'Extended Images', '0', '0', 'extendedImages', 'module', 0, '0', 1, 1, 0, NULL, 0, '0', 0, NULL),
+(74, 'Image Galley', '0', '0', 'xImageGallery', 'application', 0, '0', 1, 1, 0, NULL, 0, 'https://github.com/xepan/xImageGallery', 1, NULL),
+(75, 'xMarketingCampaign', '0', '0', 'xMarketingCampaign', 'application', 0, '0', 1, 0, 0, NULL, 0, 'https://github.com/xepan/xMarketingCampaign', 0, NULL),
+(76, 'Menus', '0', '0', 'xMenus', 'application', 0, '0', 1, 1, 0, NULL, 0, '0', 0, NULL),
+(77, 'xShop', '0', '0', 'xShop', 'application', 0, '0', 1, 1, 0, NULL, 0, 'https://github.com/xepan/xShop', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -213,15 +232,16 @@ CREATE TABLE IF NOT EXISTS `epan_components_plugins` (
   `is_system` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_component_id` (`component_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=57 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=55 ;
 
 --
 -- Dumping data for table `epan_components_plugins`
 --
 
 INSERT INTO `epan_components_plugins` (`id`, `component_id`, `name`, `event`, `params`, `is_system`) VALUES
-(55, 51, 'RemoveContentEditable', 'content-fetched', '$page', 1),
-(56, 51, 'RunServerSideComponent', 'content-fetched', '$page', 1);
+(53, 51, 'RemoveContentEditable', 'content-fetched', '$page', 1),
+(54, 51, 'RunServerSideComponent', 'content-fetched', '$page', 1),
+(52, 70, 'ImplementIntelligence', 'beforeTemplateInit', '$page', 0);
 
 -- --------------------------------------------------------
 
@@ -240,82 +260,45 @@ CREATE TABLE IF NOT EXISTS `epan_components_tools` (
   `order` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_component_id` (`component_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=240 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=233 ;
 
 --
 -- Dumping data for table `epan_components_tools`
 --
 
 INSERT INTO `epan_components_tools` (`id`, `component_id`, `name`, `is_serverside`, `is_sortable`, `is_resizable`, `display_name`, `order`) VALUES
-(231, 51, 'Template Content Region', 0, 1, 0, NULL, NULL),
-(232, 51, 'Column', 0, 1, 0, NULL, NULL),
-(233, 51, 'Row', 0, 1, 0, NULL, NULL),
-(234, 51, 'Container', 0, 1, 0, NULL, NULL),
-(235, 51, 'Image', 0, 0, 0, NULL, NULL),
-(236, 51, 'Title', 0, 0, 0, NULL, NULL),
-(237, 51, 'Text', 0, 0, 0, NULL, NULL),
-(238, 51, 'Html Block', 0, 0, 0, NULL, NULL),
-(239, 51, 'User Panel', 1, 0, 0, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `epan_data_grabber`
---
-
-CREATE TABLE IF NOT EXISTS `epan_data_grabber` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `epan_id` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `site_url` varchar(255) DEFAULT NULL,
-  `query_parameter` varchar(255) DEFAULT NULL,
-  `paginator_parameter` varchar(255) DEFAULT NULL,
-  `paginator_initial_value` varchar(255) DEFAULT NULL,
-  `records_per_page` varchar(255) DEFAULT NULL,
-  `paginator_based_on` varchar(255) DEFAULT NULL,
-  `extra_url_parameters` varchar(255) DEFAULT NULL,
-  `required_pause_between_hits` varchar(255) DEFAULT NULL,
-  `result_selector` varchar(255) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `last_run_at` datetime DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT NULL,
-  `result_format` varchar(255) DEFAULT NULL,
-  `json_url_key` varchar(255) DEFAULT NULL,
-  `reg_ex_on_href` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_epan_id` (`epan_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `epan_data_grabber`
---
-
-INSERT INTO `epan_data_grabber` (`id`, `epan_id`, `name`, `site_url`, `query_parameter`, `paginator_parameter`, `paginator_initial_value`, `records_per_page`, `paginator_based_on`, `extra_url_parameters`, `required_pause_between_hits`, `result_selector`, `created_at`, `last_run_at`, `is_active`, `result_format`, `json_url_key`, `reg_ex_on_href`) VALUES
-(1, 1, 'http://www.google.co.in', 'http://google.com/search', 'q', 'start', '0', '10', 'records', '', '10', '#ires', '2014-10-20 15:16:16', '2014-10-20 20:21:40', 1, NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `epan_data_search_phrase`
---
-
-CREATE TABLE IF NOT EXISTS `epan_data_search_phrase` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `data_grabber_id` int(11) DEFAULT NULL,
-  `subscription_category_id` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `max_record_visit` varchar(255) DEFAULT NULL,
-  `max_domain_depth` varchar(255) DEFAULT NULL,
-  `max_page_depth` varchar(255) DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT NULL,
-  `page_parameter_start_value` varchar(255) DEFAULT NULL,
-  `page_parameter_max_value` varchar(255) DEFAULT NULL,
-  `last_page_checked_at` datetime DEFAULT NULL,
-  `content_provided` text,
-  PRIMARY KEY (`id`),
-  KEY `fk_data_grabber_id` (`data_grabber_id`),
-  KEY `fk_subscription_category_id` (`subscription_category_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+(229, 77, 'ProductDetail', 1, 0, 0, '', 0),
+(220, 73, 'xSVG', 0, 0, 0, '', 0),
+(218, 72, 'ThumbnailSlider', 1, 0, 0, '', 0),
+(208, 51, 'User Panel', 1, 0, 0, NULL, NULL),
+(209, 70, 'IntelligentBlock', 0, 1, 0, 'IBlock', 0),
+(207, 51, 'Html Block', 0, 0, 0, NULL, NULL),
+(206, 51, 'Text', 0, 0, 0, NULL, NULL),
+(205, 51, 'Title', 0, 0, 0, NULL, NULL),
+(204, 51, 'Image', 0, 0, 0, NULL, NULL),
+(203, 51, 'Container', 0, 1, 0, NULL, NULL),
+(202, 51, 'Row', 0, 1, 0, NULL, NULL),
+(212, 69, 'CustomeForm', 1, 0, 0, '', 0),
+(211, 69, 'SubscriptionModule', 1, 0, 0, '', 0),
+(210, 69, 'EnquiryForm', 0, 0, 0, '', 0),
+(201, 51, 'Column', 0, 1, 0, NULL, NULL),
+(200, 51, 'Template Content Region', 0, 1, 0, NULL, NULL),
+(232, 71, 'Marquee', 0, 1, 0, '', 0),
+(217, 72, 'AwesomeSlider', 1, 0, 0, '', 0),
+(216, 72, 'BootStrap Carousel', 0, 0, 0, '', 0),
+(215, 72, 'WaterWheelCarousel', 1, 0, 0, '', 0),
+(214, 72, 'TransformGallery', 1, 0, 0, '3D transforms', 0),
+(219, 73, 'Image With Description 1', 0, 0, 0, '', 0),
+(221, 74, 'GoogleImageGallery', 1, 0, 0, '', 0),
+(230, 76, 'Bootstrap Menus', 0, 0, 0, '', 0),
+(228, 77, 'Product', 1, 0, 0, '', 0),
+(227, 77, 'Category', 1, 0, 0, '', 0),
+(226, 77, 'xCart', 1, 0, 0, '', 0),
+(225, 77, 'Search', 1, 0, 0, '', 0),
+(224, 77, 'Checkout', 1, 0, 0, '', 0),
+(223, 77, 'Member Account', 1, 0, 0, '', 0),
+(222, 77, 'Add Block', 1, 0, 0, '', 0),
+(231, 69, 'unSubscribe', 1, 0, 0, 'Un Subscriber', 0);
 
 -- --------------------------------------------------------
 
@@ -331,7 +314,23 @@ CREATE TABLE IF NOT EXISTS `epan_installed_components` (
   `params` varchar(255) DEFAULT NULL,
   `installed_on` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+
+--
+-- Dumping data for table `epan_installed_components`
+--
+
+INSERT INTO `epan_installed_components` (`id`, `epan_id`, `component_id`, `enabled`, `params`, `installed_on`) VALUES
+(23, 1, 67, 1, NULL, '2014-09-28'),
+(25, 1, 69, 1, NULL, '2014-10-06'),
+(26, 1, 70, 1, NULL, '2014-10-17'),
+(27, 1, 71, 1, NULL, '2014-10-24'),
+(28, 1, 72, 1, NULL, '2014-10-24'),
+(29, 1, 73, 1, NULL, '2014-10-24'),
+(30, 1, 74, 1, NULL, '2014-10-24'),
+(31, 1, 75, 1, NULL, '2014-10-24'),
+(32, 1, 76, 1, NULL, '2014-10-24'),
+(33, 1, 77, 1, NULL, '2014-10-24');
 
 -- --------------------------------------------------------
 
@@ -365,7 +364,7 @@ CREATE TABLE IF NOT EXISTS `epan_page` (
 --
 
 INSERT INTO `epan_page` (`id`, `parent_page_id`, `name`, `menu_caption`, `epan_id`, `is_template`, `title`, `description`, `keywords`, `content`, `body_attributes`, `created_on`, `updated_on`, `access_level`, `template_id`) VALUES
-(1, 0, 'home', 'Home', 1, 0, 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', 'World''s best and easiest cms :)', 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', '', NULL, NULL, '2014-10-25 17:18:04', 'public', 1);
+(1, 0, 'home', 'Home', 1, 0, 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', 'World''s best and easiest cms :)', 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', '', '; cursor: default;', NULL, '2014-11-02 14:23:58', 'public', 1);
 
 -- --------------------------------------------------------
 
@@ -385,14 +384,7 @@ CREATE TABLE IF NOT EXISTS `epan_page_snapshots` (
   `content` text,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `epan_page_snapshots`
---
-
-INSERT INTO `epan_page_snapshots` (`id`, `epan_page_id`, `created_on`, `updated_on`, `title`, `keywords`, `description`, `body_attributes`, `content`, `name`) VALUES
-(1, 1, '2014-09-29 10:00:31', '2014-09-29 10:00:31', 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', 'xEpan CMS, an innovative approach towards Drag And Drop CMS.', 'World''s best and easiest cms :)', 'cursor: auto; overflow: auto; background-color: rgb(255, 255, 255);', '<div id="08dad680-8429-4da9-b706-d3160de0d8dd" component_namespace="baseElements" component_type="Container" class="epan-container  epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" style=" ">   <div id="4f31de8a-0ddb-46d7-f93a-cd2dc3fe2a65" component_namespace="baseElements" component_type="Title" class="epan-component component-outline" style=" ">   <h3 class="editor editor-attached" contenteditable="true">Container in no IBlock<br>\n</h3> </div>\n</div>\n<div id="e843134f-e3bf-43de-ab35-4bb091565305" data-dimesion-id="1" component_namespace="Ai" component_type="IntelligentBlock" class="epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" style=" "><div id="005eafc4-3fac-4c89-a79a-07adf69d421b" component_namespace="baseElements" component_type="Row" class="row  epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" style=" ">    <div id="4d4162aa-1db2-41d7-aff9-93da30b1e1c8" component_namespace="baseElements" component_type="Column" class="col-md-4  epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" span="4" style=" ">   <div id="a755d5c2-0d3d-4e3b-fa6a-4f7dd9fa4697" data-dimesion-id="1" component_namespace="Ai" component_type="IntelligentBlock" class="epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" style=" ">   <div id="47971366-fd18-41cf-baf0-8c19a8a9c936" component_namespace="baseElements" component_type="Title" class="epan-component  component-outline" style=" ">  <h3 class="editor" contenteditable="true">This is Title 1<br></h3> </div></div></div><div id="46229782-f5e1-4008-f5a8-3f2353e944a3" component_namespace="baseElements" component_type="Column" class="col-md-4  epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" span="4" style=" ">   <div id="7e467c96-8b48-4b31-ef29-2f13e96b278c" data-dimesion-id="1" component_namespace="Ai" component_type="IntelligentBlock" class="epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" style=" ">   <div id="67043a51-88f8-449f-9285-c51375cb8c3f" component_namespace="baseElements" component_type="Title" class="epan-component  component-outline" style=" ">  <h3 class="editor" contenteditable="true">This is Title 2<br></h3> </div></div></div><div id="451e844c-571c-4c7a-d082-5ae54ef2b015" component_namespace="baseElements" component_type="Column" class="col-md-4  epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" span="4" style=" ">   <div id="e6f6b921-343c-4e6f-964c-a7a292bb2d73" data-dimesion-id="1" component_namespace="Ai" component_type="IntelligentBlock" class="epan-sortable-component epan-component  ui-sortable component-outline epan-sortable-extra-padding" style=" ">   <div id="813e7c3e-77ff-47e7-e63c-d7eb2748489c" component_namespace="baseElements" component_type="Title" class="epan-component  component-outline" style=" ">  <h3 class="editor" contenteditable="true">This is Title 3<br></h3> </div></div></div></div></div>', 'A');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
