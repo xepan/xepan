4a1bbdb058a5fbfbdfab477f80d92cdbf927048e
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-20 15:40:53 +0530

    Popline Icons BUG Fixed

--- page/index.php
+++ page/index.php
@@ -66,6 +66,7 @@ class page_index extends Page {
 			$this->api->template->appendHTML('js_include','<link type="text/css" href="templates/js/popline/css/normalize.css" rel="stylesheet" />'."\n");
 			$this->api->template->appendHTML('js_include','<link type="text/css" href="templates/js/popline/themes/default.css" rel="stylesheet" />'."\n");
 			$this->api->template->appendHTML('js_include','<script src="templates/js/popline/build/jquery.popline.min.js"></script>'."\n");
+			$this->api->template->appendHTML('js_include','<link type="text/css" href="templates/js/popline/font-awesome/css/font-awesome.min.css" rel="stylesheet" />'."\n");
 
 			// Font Awsome
 			$this->api->template->appendHTML('js_include','<link type="text/css" href="templates/default/font-awesome/css/font-awesome.min.css" rel="stylesheet" />'."\n");
