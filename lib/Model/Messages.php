e3aa046faf8e8f71273b30d09a8f6cf558fe3f44
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-21 12:17:07 +0530

    Message create function

--- lib/Model/Messages.php
+++ lib/Model/Messages.php
@@ -7,9 +7,18 @@ class Model_Messages extends Model_Table{
 		$this->addField('name')->caption('Title');
 		$this->addField('message')->type('text');
 		$this->addField('created_at')->type('datetime')->defaultValue(date('Y-m-d H:i:s'));
-		$this->addField('is_read')->type('boolean');
+		$this->addField('is_read')->type('boolean')->defaultValue(false);
 		$this->addField('sender_signature');
 
 		// $this->add('dynamic_model/Controller_AutoCreator');
+	}
+
+	function createNew($epan_id,$title,$message,$sender_signature){
+
+		$this['epan_id']=$epan_id;
+		$this['name']=$title;
+		$this['message']=$message;
+		$this['sender_signature']=$sender_signature;
+		$this->save();
 	}
 }
