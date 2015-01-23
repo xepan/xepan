1e300a542430e2074abf301ddbe9aa9c16384272
xavocvijay <guddavijay@gmail.com>
2015-01-23 18:40:27 +0530

    Modified Message Detaisl

--- page/owner/messagedetails.php
+++ page/owner/messagedetails.php
@@ -12,12 +12,26 @@ class page_owner_messagedetails extends page_base_owner{
 		}
 			
 		$this->add('HR');
-		$v = $this->app->layout->add('View_Info')->set('Message Detail');		
-		$this->add('View')->set('Name - '.$msg['name']);
-		$this->add('View')->set('From - '.$msg['sender_signature']);
-		$this->add('View')->set('Name Space - '.$msg['sender_namespace']);
-		$this->add('View')->set('Message - '.$msg['message']);
+		$col=$this->add('Columns');
+		$msg_detail=$col->addColumn(6);
+		$contributers_detail=$col->addColumn(6);
 
+		$v = $msg_detail->app->layout->add('View_Info')->set('Message Detail');		
+		$msg_detail->add('View')->set('Name - '.$msg['name']);
+		$msg_detail->add('View')->set('From - '.$msg['sender_signature']);
+		$msg_detail->add('View')->set('Name Space - '.$msg['sender_namespace']);
+		$msg_detail->add('View')->set('Message - '.$msg['message']);
+
+		$custome_field=$this->add('xEnquiryNSubscription/Model_CustomFields');
+		
+		$form_values="";
+		foreach ($custome_field as $junk) {
+				$form_values .= "<b>".$custome_field['name']."</b> : " . $custome_field['name'] . '<br/>';
+				if($junk['type'] == 'email'){
+						$email = $form[$this->api->normalizeName($custome_field['name'])];
+					}
+		}
+			$contributers_detail->add('View')->setHTML($form_values);
 		
 	}
 }		
