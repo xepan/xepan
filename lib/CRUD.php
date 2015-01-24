5a7318e6a57173fe5de296e0a0a1ee5b60a37380
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-01-24 11:36:22 +0530

    setModel Bug fixed

--- lib/CRUD.php
+++ lib/CRUD.php
@@ -3,7 +3,7 @@
 class CRUD extends View_CRUD{
 	public $form_class='Form_Stacked';
 	
-	function setModel($model,$f=array(),$f2=array()){
+	function setModel($model,$f=null,$f2=null){
 		parent::setModel($model,$f,$f2);
 		$this->add('Controller_FormBeautifier');
 	}
