c0700ca688cbab3e49f2c5495ed77bdd0527edca
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-02-19 12:57:21 +0530

    user menu added in all application and leave and attendence model added in HR

--- epan-components/xShop/lib/Menu/User.php
+++ epan-components/xShop/lib/Menu/User.php
@@ -6,6 +6,6 @@ class Menu_User extends \Menu_Vertical{
 	function init(){
 		parent::init();
 		$this->addItem('Dashboard','xProduction_page_owner_user_dashboard');
-		$this->addItem('Sales work','xProduction_page_owner_user_jobcards');
+		$this->addItem('My Task','xProduction_page_owner_user_mytask');
 	}
 }
