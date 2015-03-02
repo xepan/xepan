8e1d329b1b4d4fca04252cd135835759ce69d0f6
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-02 17:30:45 +0530

    department condition to store status

--- epan-components/xStore/page/owner/materialrequest/assigned.php
+++ epan-components/xStore/page/owner/materialrequest/assigned.php
@@ -3,8 +3,11 @@
 class page_xStore_page_owner_materialrequest_assigned extends page_xStore_page_owner_main{
 	function init(){
 		parent::init();
+		$store = $this->add('xHR/Model_Department')->loadBy('related_application_namespace','xStore');
 
+		$model = $this->add('xStore/Model_MaterialRequest_Assigned');
+		$model->addCondition('department_id',$store->id);
 		$crud=$this->add('CRUD');
-		$crud->setModel('xStore/Model_MaterialRequest_Assigned');
+		$crud->setModel($model);
 	}
 }
--- epan-components/xStore/page/owner/materialrequest/complete.php
+++ epan-components/xStore/page/owner/materialrequest/complete.php
@@ -3,9 +3,12 @@
 class page_xStore_page_owner_materialrequest_complete extends page_xStore_page_owner_main{
 	function init(){
 		parent::init();
+		$store = $this->add('xHR/Model_Department')->loadBy('related_application_namespace','xStore');
 
+		$model = $this->add('xStore/Model_MaterialRequest_Completed');
+		$model->addCondition('department_id',$store->id);
 		$crud=$this->add('CRUD');
-		$crud->setModel('xStore/Model_MaterialRequest_Completed');
+		$crud->setModel($model);
 	}
 	
 }
