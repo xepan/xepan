5320652c0d7d44ba97514c1a30065fb1e429438a
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-09 17:15:07 +0530

    dept to hr

--- epan-components/xHR/page/owner/department/basic.php
+++ epan-components/xHR/page/owner/department/basic.php
@@ -4,13 +4,13 @@ class page_xHR_page_owner_department_basic extends Page{
 	function init(){
 		parent::init();
 
-		if(!$_GET['department_id'])
-			throw new \Exception($_GET['department_id']);
+		if(!$_GET['hr_department_id'])
+			throw new \Exception($_GET['hr_department_id']);
 			
 			// return;
 		
-		$this->api->stickyGET('department_id');
-		$selected_dept_model = $this->add('xHR/Model_Department')->load($_GET['department_id']);		
+		$this->api->stickyGET('hr_department_id');
+		$selected_dept_model = $this->add('xHR/Model_Department')->load($_GET['hr_department_id']);		
 		if(!$selected_dept_model->loaded())
 			return;
 		
