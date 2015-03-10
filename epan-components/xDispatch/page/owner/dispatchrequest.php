488486676ea64c6f4635f4e1ba564d6a075845c1
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-10 10:42:51 +0530

    dispatch req page created

---
+++ epan-components/xDispatch/page/owner/dispatchrequest.php
@@ -0,0 +1,10 @@
+<?php
+class page_xDispatch_page_owner_dispatchrequest extends page_xDispatch_page_owner_main {
+	function init(){
+		parent::init();
+		
+		$crud=$this->app->layout->add('CRUD');
+		$crud->setModel('DispatchRequest')
+	}
+
+}
