5eefbbc888135d1cb06bfa1782924c6220121570
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-10 15:37:39 +0530

    refine acl according post

--- epan-components/xShop/lib/Model/Quotation/Approved.php
+++ epan-components/xShop/lib/Model/Quotation/Approved.php
@@ -3,6 +3,13 @@
 namespace xShop;
 
 class Model_Quotation_Approved extends Model_Quotation{
+	public $actions=array(
+			'can_view'=>array('caption'=>'Whose created Quotation(approved) this post can see'),
+			'allow_edit'=>array('caption'=>'Whose created Quotation(approved) this post can edit'),
+			'allow_add'=>array('caption'=>'Can this post create Quotation(approved)'),
+			'allow_del'=>array('caption'=>'Whose Created Quotation(approved) this post can delete'),
+			'can_approve'=>array('caption'=>'Whose Created Quotation(approved) this post can approved'),
+		);
 
 	function init(){
 		parent::init();
--- epan-components/xShop/lib/Model/Quotation/Redesign.php
+++ epan-components/xShop/lib/Model/Quotation/Redesign.php
@@ -5,12 +5,12 @@ namespace xShop;
 class Model_Quotation_Redesign extends Model_Quotation{
 
 	public $actions=array(
-			'can_view'=>array('caption'=>'Whose created Redesign this post can see'),
-			'allow_edit'=>array('caption'=>'Whose created Redesign this post can edit'),
-			'allow_add'=>array('caption'=>'Can this post create Redesign'),
-			'allow_del'=>array('caption'=>'Whose Created Redesign this post can delete'),
-			'can_submit'=>array('caption'=>'Whose Created Redesign this post can submit'),
-			'can_cancel'=>array('caption'=>'Whose Created Redesign this post can cancel'),
+			'can_view'=>array('caption'=>'Whose created Quotation(Redesign) this post can see'),
+			'allow_edit'=>array('caption'=>'Whose created Quotation(Redesign) this post can edit'),
+			'allow_add'=>array('caption'=>'Can this post create Quotation(Redesign)'),
+			'allow_del'=>array('caption'=>'Whose Created Quotation(Redesign) this post can delete'),
+			'can_submit'=>array('caption'=>'Whose Created Quotation(Redesign) this post can submit'),
+			'can_cancel'=>array('caption1'=>'Whose Created Quotation(Redesign) this post can cancel'),
 		);
 
 	function init(){
