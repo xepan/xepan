7463160ec2127b040350497742dcd12c974e7a81
xavocvijay <guddavijay@gmail.com>
2015-03-13 14:54:31 +0530

    Acl Added On Task

--- epan-components/xProduction/lib/Model/Task/Assigned.php
+++ epan-components/xProduction/lib/Model/Task/Assigned.php
@@ -2,6 +2,10 @@
 namespace xProduction;
 
 class Model_Task_Assigned extends Model_Task{
+	public $actions=array(
+			'can_view'=>array('caption'=>'Whose created Jobcard this post can see'),
+			'can_start_processing'=>array('caption'=>'Whose Created  this post can Processing')
+		);
 	function init(){
 		parent::init();
 
