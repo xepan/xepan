ae62afc2ca3cd73a935873daa878a18642a8abb6
sinharakesh <rksinha.btech@gmail.com>
2015-03-13 10:19:44 +0530

    dashboard page added

---
+++ epan-components/xPurchase/page/owner/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+class page_xPurchase_page_owner_dashboard extends page_xPurchase_page_owner_main {
+	
+	function initMainPage(){
+		$this->add('H1')->set('Purchase Dashboard Comming Soon..... on xEpan');
+	}
+
+}
