a5b66f38a90b209d9e2739b28c2b02c694a649e8
xavocvijay <guddavijay@gmail.com>
2015-03-17 15:06:26 +0530

    modofied View Jobcard

--- epan-components/xProduction/lib/View/Jobcard.php
+++ epan-components/xProduction/lib/View/Jobcard.php
@@ -20,19 +20,15 @@ class View_Jobcard extends \View{
 		$this->template->set('member',$order['member']);
 		$this->template->set('order_from',$order['order_from']);
 		$this->template->set('on_date',$order['on_date']);
-		$this->template->set('status',$order['status']);
+		$this->template->set('status',$this->jobcard['status']);
 		$this->template->set('from_dept',$this->jobcard['from_department']);
 		$this->template->set('to_dept',$this->jobcard['to_department']);
-		$this->template->set('name',$this->jobcard['name']);
+		$this->template->set('name',$this->jbocard['name']);
 		$this->current_row['sno']=$this->sno;
 		$this->current_row['current_status'] = $this->model->getCurrentStatus();
 		$this->current_row['custom_fields'] = $this->model->ref('item_id')->genericRedableCustomFieldAndValue($this->model['custom_fields']);
 		$this->sno++;
 	}
-
-	
-
-	
 		
 	function defaultTemplate(){
 		$this->app->pathfinder->base_location->addRelativeLocation(
