3c2aaac09b8f5060dd8b37fd965a781fec4d8247
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-17 20:34:38 +0530

    Salary template conditioned Jobcard call function instead setting status directly

--- epan-components/xHR/page/owner/department/salarytemplate.php
+++ epan-components/xHR/page/owner/department/salarytemplate.php
@@ -9,14 +9,21 @@ class page_xHR_page_owner_department_salarytemplate extends page_xHR_page_owner_
 			$salary_temp_amount=$col->addColumn(6);
 
 			$salarytem=$this->add('xHR/Model_SalaryTemplate');
+			$salarytem->addCondition('department_id',$this->api->stickyGET('hr_department_id'));
+
+
 			$crud=$salary_temp->add('CRUD');
-				$crud->setModel($salarytem);
-				$crud->addref('xHR/TemplateSalary',array('label'=>'Template Salary'));
+			$crud->setModel($salarytem);
+			$crud->addref('xHR/TemplateSalary',array('label'=>'Template Salary'));
+
 			if(!$crud->isEditing()){
 				$g=$crud->grid;
 				$g->addPaginator(15);
 				$g->addQuickSearch(array('name'));
 			}
 
+			if($crud->isEditing()){
+				// $crud->form->getElement('post_id')->getModel()->addCondition('department_id',$_GET['hr_department_id']);
+			}
 	}
 }
