95f93c74b5f38194346963353888faa8af4508e2
xavocvijay <guddavijay@gmail.com>
2015-03-17 13:18:16 +0530

    Material Request Item Details work almost done

--- epan-components/xStore/lib/View/MaterialRequest.php
+++ epan-components/xStore/lib/View/MaterialRequest.php
@@ -3,26 +3,25 @@ namespace xStore;
 class View_MaterialRequest extends \CompleteLister{
 	
 	public $materialrequest;
+	public $sno=1;
 	
 	function init(){
 		parent::init();
+		// $this->add('View_Info')->set('Material Request Details Here');
 
-		$this->add('View_Info')->set('Material Request Details Here');
-
-		$this->template->set('order_on',$this->materialrequest['on_date']);
-		$this->template->set('status',$this->materialrequest['status']);
+		$this->template->set('request_on',$this->materialrequest['created_at']);
+		$this->template->set('status',ucwords($this->materialrequest['status']));
+		$this->template->set('form_dept',$this->materialrequest['from_department']);
+		$this->template->set('to_dept',$this->materialrequest['to_department']);
 	
 		$this->setModel($this->materialrequest->itemrows());
-	
 	}
 
 	function formatRow(){
-		// $this->current_row['sno']=$this->sno;
-		$this->current_row['custom_field'] = "sfsdf";
-		// $this->sno++;
+		$this->current_row['sno']=$this->sno;
+		$this->current_row_html['custom_field'] =  $this->add('xShop/Model_Item')->genericRedableCustomFieldAndValue($this->model['custom_fields']);
+		$this->sno++;
 	}
-
-	
 
 	function defaultTemplate(){
 		$this->app->pathfinder->base_location->addRelativeLocation(
@@ -33,8 +32,6 @@ class View_MaterialRequest extends \CompleteLister{
 		        'js'=>'templates/js',
 		    )
 		);
-		
-		
 		return array('view/xStore-materialrequest');
 	}
 }
