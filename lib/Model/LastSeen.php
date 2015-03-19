c06a502c8100c53e8b84cd2c0f928e900087926a
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-19 13:08:44 +0530

    Acl Based Counting of Documents

---
+++ lib/Model/LastSeen.php
@@ -0,0 +1,19 @@
+<?php
+
+class Model_LastSeen extends SQL_Model {
+	public $table = "last_seen_updates";
+
+	function init(){
+		parent::init();
+
+		$this->hasOne('xHR/Employee','employee_id');
+
+		$this->addField('related_document_id');
+		$this->addField('related_root_document_name');
+		$this->addField('related_document_name');
+
+		$this->addField('seen_till')->type('datetime')->defaultValue('1970-01-01');
+
+		$this->add('dynamic_model/Controller_AutoCreator');
+	}
+}
