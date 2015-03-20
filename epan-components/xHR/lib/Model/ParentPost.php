1e44bfc5f2939149c0d697943e75432196af677f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-20 12:19:38 +0530

    Parent POst with department

--- epan-components/xHR/lib/Model/ParentPost.php
+++ epan-components/xHR/lib/Model/ParentPost.php
@@ -4,4 +4,16 @@ namespace xHR;
 
 class Model_ParentPost extends Model_Post {
 	var $table_alias= "pPost";
+	var $title_field ='post_with_department';
+	
+	function init(){
+		parent::init();
+
+		$this->addExpression('post_with_department')->set($this->dsql()->concat(
+			$this->getElement('name'),
+			' :: ',
+			$this->add('xHR/Model_Department')->addCondition('id',$this->getElement('department_id'))->fieldQuery('name')
+			));
+
+	}
 }
