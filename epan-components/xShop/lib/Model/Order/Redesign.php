fe2a2d9d0ad2e08fe3811380c37b0e4d97f157ef
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-20 19:06:54 +0530

    Typo

---
+++ epan-components/xShop/lib/Model/Order/Redesign.php
@@ -0,0 +1,17 @@
+<?php
+namespace xShop;
+
+class Model_Order_Redesign extends Model_Order{
+
+	public $actions=array(
+			'can_view'=>array('caption'=>'Whose created Order(approved) this post can see'),
+			'can_reject'=>array('caption'=>'Can this post Reject Order(approved)')
+		);
+	
+
+	function init(){
+		parent::init();
+
+		$this->addCondition('status','redesign');
+	}
+}
