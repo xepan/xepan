f27dc26e8a842506803afe88777ba6f08ce52608
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-20 23:26:09 +0530

    Orer grid beautify

--- epan-components/xShop/lib/Grid/Order.php
+++ epan-components/xShop/lib/Grid/Order.php
@@ -3,47 +3,52 @@
 namespace xShop;
 
 class Grid_Order extends \Grid {
-
+	public $vp;
 	function init(){
 		parent::init();
 		$self = $this;
 		
 		$this->addSno();
 
-		$vp = $this->add('VirtualPage')->set(function($p)use($self){
+		$this->vp = $this->add('VirtualPage')->set(function($p)use($self){
 			$o = $p->add('xShop/Model_Order')->load($_GET['sales_order_clicked']);
 			$order = $p->add('xShop/View_Order');
 			$order->setModel($o);
 		});
-		
-		$this->js(true)->find('tr td:nth-child(1)')->css(array('cursor'=>'pointer'));
-		$this->on('click','tbody td:nth-child(1)',$this->js()->univ()->frameURL('Sales Order',array($this->api->url($vp->getURL()),'sales_order_clicked'=>$this->js()->_selectorThis()->closest('tr')->data('id'))));
 	}
 
+	function format_view($field){
+		if($this->model['orderitem_count']==0){
+			$this->setTDParam($field, 'class', ' atk-swatch-yellow ');
+		}else{
+			$this->setTDParam($field, 'class', '');
+		}
+		$this->current_row_html[$field] = '<a href="#na" onclick="javascript:'.$this->js()->univ()->frameURL('Sale Order', $this->api->url($this->vp->getURL(),array('sales_order_clicked'=>$this->model->id))).'">'. $this->current_row[$field] ."</a>";
+	}
 
 	function setModel($model,$fields=null){
 
 		if($fields==null){
 			$fields = array(
 						'name','order_from','on_date','member',
-						'net_amount','last_action'
+						'net_amount','last_action','orderitem_count'
 						);
 		}
 
 		$m = parent::setModel($model,$fields);
 
 		$this->addColumn('expander','details',array('page'=>'xShop_page_owner_order_detail','descr'=>'Details'));
-		$this->addColumn('Button','print');
 
-		if($_GET['print']){			
-			$this->js()->univ()->newWindow($this->api->url("xShop/page_printorder",array('order_id'=>$_GET['print'],'cut_page'=>1,'subpage'=>'xshop-junk')),null,'height=689,width=1246,scrollbar=1')->execute();
-		} 
+		// $this->addColumn('Button','print');
+		// if($_GET['print']){			
+		// 	$this->js()->univ()->newWindow($this->api->url("xShop/page_printorder",array('order_id'=>$_GET['print'],'cut_page'=>1,'subpage'=>'xshop-junk')),null,'height=689,width=1246,scrollbar=1')->execute();
+		// } 
 
-
+		$this->addFormatter('name','view');
+		$this->removeColumn('orderitem_count');
 
 		$this->addPaginator(100);
 		$this->addQuickSearch(array('order_id','order_from','on_date','discount_voucher'));
 		return $m;
-	}	
-
+	}
 }
