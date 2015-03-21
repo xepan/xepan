439369853f3613cc10c7a3c69da91562bd8b0d0b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-21 17:07:50 +0530

    Grid to order

--- epan-components/xProduction/lib/Grid/JobCard.php
+++ epan-components/xProduction/lib/Grid/JobCard.php
@@ -8,18 +8,13 @@ class Grid_JobCard extends \Grid{
 		$self= $this;
 		$this->addSno();
 
-<<<<<<< HEAD
-		$this->add('VirtualPage')->addColumn('col_name','Job Card','btn_text',$this)->set(function($p)use($self){
-			$p->add('xProduction/View_Jobcard',array('jobcard'=>$p->add('xProduction/Model_JobCard')->load($p->id)));
-=======
 		$this->vp=$this->add('VirtualPage')->set(function($p)use($self){
-			$p->add('xProduction/View_Jobcard',array('jobcard'=>$p->add('xProduction/Model_JobCard')->load($_GET['jobcard_clicked'])));
->>>>>>> 613778b33610d5e80dbd09f570226ff3f314548b
+			$p->add('xProduction/View_Jobcard',array('jobcard'=>$p->add('xProduction/Model_JobCard')->load($this->api->stickyGET('jobcard_clicked'))));
 		});
 	}
 	
 	function format_view($field){
-		$this->current_row_html[$field] = '<a href="#na" onclick="javascript:'.$this->js()->univ()->frameURL('Sale Order', $this->api->url($this->vp->getURL(),array('jobcard_clicked'=>$this->model->id))).'">'. $this->current_row[$field] ."</a>";
+		$this->current_row_html[$field] = '<a href="#na" onclick="javascript:'.$this->js()->univ()->frameURL('Job Card', $this->api->url($this->vp->getURL(),array('jobcard_clicked'=>$this->model->id))).'">'. $this->current_row[$field] ."</a>";
 	}
 
 
