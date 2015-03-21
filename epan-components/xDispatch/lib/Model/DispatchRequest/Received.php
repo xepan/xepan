ba8fc40121ef00fbfaa2f37ab340d1c78dc74a40
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-21 19:40:37 +0530

    Dispatch request received can be mark_processed

--- epan-components/xDispatch/lib/Model/DispatchRequest/Received.php
+++ epan-components/xDispatch/lib/Model/DispatchRequest/Received.php
@@ -8,6 +8,7 @@ class Model_DispatchRequest_Received extends Model_DispatchRequest{
 			'allow_edit'=>array('caption'=>'Whose created Jobcard this post can edit'),
 			'can_assign'=>array('caption'=>'Whose created Jobcard(received) this post can assign'),
 			'can_assign_to'=>array('caption'=>' Whose created Jobcard(received) this post can assign to'),
+			'can_mark_processed'=>array('caption'=>' Whose created Jobcard(received) this post can assign to'),
 		);
 	function init(){
 		parent::init();
