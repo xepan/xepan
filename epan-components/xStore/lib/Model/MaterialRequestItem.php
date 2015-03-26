ec28f7b8e4c7e70715f1be2daeed4cbc8770252e
sinharakesh <rksinha.btech@gmail.com>
2015-03-26 16:52:33 +0530

    validation changed

--- epan-components/xStore/lib/Model/MaterialRequestItem.php
+++ epan-components/xStore/lib/Model/MaterialRequestItem.php
@@ -27,21 +27,18 @@ class Model_MaterialRequestItem extends \Model_Document{
 	function beforeSave(){
 
 		// validate custom field entries
-		$phase = $this->add('xHR/Model_Department')->loadStore();
-
 		if($this['custom_fields']==''){
-			// $phases_ids = $this->ref('item_id')->getAssociatedDepartment();
 			$cust_field_array = array();
 		}else{
-			$cust_field_array = json_decode($this['custom_fields'],true);
-			// $phases_ids = array_keys($cust_field_array);
+			$all_custom_field = json_decode($this['custom_fields'],true);
+			$cust_field_array = $all_custom_field['stockeffectcustomfield'];
 		}
 
 		// foreach ($phases_ids as $phase_id) {
-			$custom_fields_assos_ids = $this->ref('item_id');
-			$custom_fields_assos_ids = $custom_fields_assos_ids->getAssociatedCustomFields($phase->id);
-			foreach ($custom_fields_assos_ids as $cf_id) {
-				if(!isset($cust_field_array[$phase->id][$cf_id]) or $cust_field_array[$phase->id][$cf_id] == ''){
+			$stock_effect_cf = $this->ref('item_id')->stockeffectcustomfields();
+			// $custom_fields_assos_ids = $custom_fields_assos_ids->getAssociatedCustomFields($phase->id);
+			foreach ($stock_effect_cf as $cf) {
+				if(!isset($cust_field_array['stockeffectcustomfield'][$cf['customfield_id']]) or $cust_field_array['stockeffectcustomfield'][$cf['customfield_id']] == ''){
 					throw $this->exception('Custom Field Values not proper','Growl');
 				}
 			}
