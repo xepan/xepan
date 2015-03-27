be0cbd7c8e291a29cdacd0e5951921f4afad74d3
sinharakesh <rksinha.btech@gmail.com>
2015-03-27 16:30:36 +0530

    DispatchRequest Grid Formmater order or dispatchrequest added

--- epan-components/xDispatch/lib/Grid/DeliveryNote.php
+++ epan-components/xDispatch/lib/Grid/DeliveryNote.php
@@ -4,8 +4,31 @@ class Grid_DeliveryNote extends \Grid{
 	function init(){
 		parent::init();
 
+		$this->vp_order = $this->add('VirtualPage')->set(function($p){
+			$o = $p->add('xShop/Model_Order')->loadBy('name',$_GET['sales_order_no_clicked']);
+			$order = $p->add('xShop/View_Order',array('show_price'=>false));
+			$order->setModel($o);
+		});
+
 		$this->addQuickSearch(array('order','warehouse'));
 		$this->addPaginator($ipp=50);
+	}
+
+	function format_orderview($field){
+		$this->current_row_html[$field] = '<a href="#na" onclick="javascript:'.$this->js()->univ()->frameURL('Order '. $this->model['order_no'], $this->api->url($this->vp_order->getURL(),array('sales_order_no_clicked'=>$this->model['order_no']))).'">'. $this->current_row[$field] ."</a>";
+	}
+
+
+	function setModel($delivery_note, $fields=null){	
+		if(!$fields)
+			$fields= array('order_no','name','created_at','from_department','forwarded_to');
+
+		$m=parent::setModel($delivery_note,$fields);
+
+		if(in_array('order_no', $fields))
+			$this->addFormatter('order_no','orderview');
+
+		return $m;
 	}
 
 }
