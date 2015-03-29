059d867470fef5d87862e62c0f845bf0ee79c0c5
sinharakesh <rksinha.btech@gmail.com>
2015-03-29 15:26:52 +0530

    Crud Payment form adde

--- epan-components/xShop/page/owner/order/draft.php
+++ epan-components/xShop/page/owner/order/draft.php
@@ -12,8 +12,49 @@ class page_xShop_page_owner_order_draft extends page_xShop_page_owner_main{
 		}
 
 		if($crud->isEditing('add') OR $crud->isEditing('edit')){
-			$crud->form->add('View_Error')->set('Payment Advanced ' . $crud->isEditing());
+			$crud->form->add('View_Info')->set('Payment Advanced ');
+			$form = $crud->form;
+			$form->addField('DropDown','payment')->setValueList(array('cheque'=>'Bank Account/Cheque','cash'=>'Cash'))->setEmptyText('Select Payment Mode');
+			$form->addField('Money','amount');
+			$form->addField('line','bank_account_detail');
+			$form->addField('line','cheque_no');
+			$form->addField('DatePicker','cheque_date');
+			$form->addField('Checkbox','send_invoice_via_email');
+			$form->addField('line','email_to');
+			// $form->addSubmit('PayNow');
+			if($form->isSubmitted()){
+				$order = $crud->model;				
+				//CHECK FOR GENERATE INVOICE
+				if($form['payment']){
+					switch ($form['payment']) {
+						case 'cheque':
+							if(trim($form['cheque_no']) =="")
+								$form->displayError('cheque_no','Cheque Number not valid.');
+
+							if(!$form['cheque_date'])
+								$form->displayError('cheque_date','Date Canot be Empty.');
+
+							if(trim($form['bank_account_detail']) == "")
+								$form->displayError('bank_account_detail','Account Number Cannot  be Null');
+						break;
+
+						default:
+							if(trim($form['amount']) == "")
+								$form->displayError('amount','Amount Cannot be Null');
+						break;
+					}
+					
+					if($form['payment'] == "cash")
+						$order->cashAdvance($form['amount']);
+					
+					if($form['payment'] == "cheque")
+						$order->bankAdvance($form['amount'],$form['cheque_no'],$form['cheque_date'],$form['bank_account_detail'],$self_bank_account=null);
+				}
+			
+			}
 		}
+
+
 
 	}
 }		
