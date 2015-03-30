544c15221904e8bcb667c30d526c65c64db38304
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-30 15:17:04 +0530

    From layout to page

--- epan-components/xDispatch/page/owner/deliverynote.php
+++ epan-components/xDispatch/page/owner/deliverynote.php
@@ -3,7 +3,7 @@ class page_xDispatch_page_owner_deliverynote extends page_xDispatch_page_owner_m
 	function init(){
 		parent::init();
 
-		$tabs=$this->app->layout->add('Tabs');
+		$tabs=$this->add('Tabs');
 		$tabs->addTabURL('xDispatch_page_owner_deliverynote_submitted','Submitted');
 		// $tabs->addTabURL('xDispatch_page_owner_deliverynote_received','Received');
 		$tabs->addTabURL('xDispatch_page_owner_deliverynote_completed','Completed');
