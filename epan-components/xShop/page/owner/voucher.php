cf43adeac6c46f53728d642f3023aadabe168ffb
sinharakesh <rksinha.btech@gmail.com>
2015-03-30 17:20:51 +0530

    discount voucher action added

--- epan-components/xShop/page/owner/voucher.php
+++ epan-components/xShop/page/owner/voucher.php
@@ -4,7 +4,7 @@ class page_xShop_page_owner_voucher extends page_xShop_page_owner_main{
 
 	function page_index(){
 
-		$crud=$this->app->layout->add('CRUD');
+		$crud=$this->add('CRUD');
 		$crud->setModel('xShop/DiscountVoucher');
 		// $crud->add('Controller_FormBeautifier');
 		if(!$crud->isEditing()){
