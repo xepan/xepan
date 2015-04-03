3ff10147c2911b9d3dbcb3778a89598416381790
sinharakesh <rksinha.btech@gmail.com>
2015-04-03 17:32:46 +0530

    allow_del added

--- epan-components/xProduction/lib/Model/Jobcard/Cancelled.php
+++ epan-components/xProduction/lib/Model/Jobcard/Cancelled.php
@@ -4,7 +4,8 @@ namespace xProduction;
 
 class Model_Jobcard_Cancelled extends Model_JobCard {
 	public $actions=array(
-			'can_view'=>array()
+			'can_view'=>array(),
+			'allow_del'=>array(),
 		);
 	
 	function init(){
