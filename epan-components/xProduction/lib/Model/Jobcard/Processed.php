fde2b4aa0b97e4f70ae5c6d5c59253d08f0de76b
sinharakesh <rksinha.btech@gmail.com>
2015-04-03 17:32:57 +0530

    cancl action added

--- epan-components/xProduction/lib/Model/Jobcard/Processed.php
+++ epan-components/xProduction/lib/Model/Jobcard/Processed.php
@@ -6,7 +6,7 @@ class Model_Jobcard_Processed extends Model_JobCard{
 	public $actions=array(
 			'can_view'=>array(),
 			'can_forward'=>array('icon'=>'forward'),
-							
+			'can_cancel'=>array(),
 		);
 	function init(){
 		parent::init();
