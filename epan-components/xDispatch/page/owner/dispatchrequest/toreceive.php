da4350a3b1d096cb2bb762bf4cc432a8f165036f
sinharakesh <rksinha.btech@gmail.com>
2015-04-07 12:40:16 +0530

    grid class add

--- epan-components/xDispatch/page/owner/dispatchrequest/toreceive.php
+++ epan-components/xDispatch/page/owner/dispatchrequest/toreceive.php
@@ -7,7 +7,8 @@ class page_xDispatch_page_owner_dispatchrequest_toreceive extends page_xDispatch
 		//In DispatchDepartment Request ToReceive according to Status Approved
 		$disp_req_item = $this->add('xDispatch/Model_DispatchRequestItem')
 			->addCondition('status','to_receive');
-		$crud=$this->add('CRUD');
+
+		$crud=$this->add('CRUD',array('grid_class'=>'xDispatch/Grid_DispatchRequestItem'));
 		$crud->setModel($disp_req_item);
 		$crud->add('xHR/Controller_Acl');
 	}
