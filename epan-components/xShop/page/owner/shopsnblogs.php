ad18f68bacfef0cd1ef526fea0438aa906fbebd8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-04-08 13:45:37 +0530

    Send Via Email Specified Tab vise Step by Step

--- epan-components/xShop/page/owner/shopsnblogs.php
+++ epan-components/xShop/page/owner/shopsnblogs.php
@@ -13,6 +13,9 @@ class page_xShop_page_owner_shopsnblogs extends page_xShop_page_owner_main {
 	}
 
 	function page_shops(){
+
+
+
 		$crud= $this->add('CRUD',array('grid_class'=>'xShop/Grid_Shop'));
 		$crud->setModel('xShop/Shop');
 
@@ -62,17 +65,65 @@ class page_xShop_page_owner_shopsnblogs extends page_xShop_page_owner_main {
 	function page_blogs_configuration(){
 		$application_id = $this->api->StickyGET('xshop_application_id');
 		$config_model=$this->add('xShop/Model_Configuration')->addCondition('application_id',$application_id)->tryLoadAny();
-		$form=$this->add('Form');
-		$form->setModel($config_model);
-		$form->addSubmit('Update');
-		if($form->Submitted()){
-			// throw new \Exception("Error Processing Request");
-			$form->Update();
-			$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
-			// $form->js()->univ()->successMessage('Update Successfully')->execute();	
-		}
-		$form->addClass('panel panel-default');
-		$form->addStyle('padding','20px');
+		$tab=$this->add('Tabs');
+			$comment_tab=$tab->addTab('Comments');
+				$comment_form=$comment_tab->add('Form');
+				$comment_form->setModel($config_model,array('disqus_code'));
+				$comment_form->addSubmit('Update');
+				if($comment_form->Submitted()){
+					$comment_form->Update();
+					$comment_form->js(null,$comment_form->js()->reload())->univ()->successMessage('Update Information')->execute();
+				}
+				$comment_form->addClass('panel panel-default');
+				$comment_form->addStyle('padding','20px');
+			
+			$mail_tab=$tab->addTab('layout');
+			
+				$lay_tab=$mail_tab->add('Tabs');
+					$sales_tab=$lay_tab->addTab('Sales Order');
+						$sales_form=$sales_tab->add('Form');
+						$sales_form->setModel($config_model,array('order_detail_email_subject','order_detail_email_body'));
+						$sales_form->addSubmit('Update');
+						if($sales_form->Submitted()){
+							$sales_form->Update();
+							$sales_form->js(null,$sales_form->js()->reload())->univ()->successMessage('Update Information')->execute();
+						}
+						$sales_form->addClass('panel panel-default');
+						$sales_form->addStyle('padding','20px');
+					
+					$purchase_tab=$lay_tab->addTab('Purchase Order');
+						$purchase_form=$purchase_tab->add('Form');
+						$purchase_form->setModel($config_model,array('purchase_order_detail_email_subject','purchase_order_detail_email_body'));
+						$purchase_form->addSubmit('Update');
+						if($purchase_form->Submitted()){
+							$purchase_form->Update();
+							$purchase_form->js(null,$purchase_form->js()->reload())->univ()->successMessage('Update Information')->execute();
+						}
+						$purchase_form->addClass('panel panel-default');
+						$purchase_form->addStyle('padding','20px');
+					
+					$quotation_tab=$lay_tab->addTab('Quotation');
+						$q_form=$quotation_tab->add('Form');
+						$q_form->setModel($config_model,array('quotation_email_subject','quotation_email_body'));
+						$q_form->addSubmit('Update');
+						if($q_form->Submitted()){
+							$q_form->Update();
+							$q_form->js(null,$q_form->js()->reload())->univ()->successMessage('Update Information')->execute();
+						}
+						$q_form->addClass('panel panel-default');
+						$q_form->addStyle('padding','20px');
+					
+					$invoice_tab=$lay_tab->addTab('Sales Invoice');
+						$i_form=$invoice_tab->add('Form');
+						$i_form->setModel($config_model,array('invoice_email_subject','invoice_email_body'));
+						$i_form->addSubmit('Update');
+						if($i_form->Submitted()){
+							$q_form->Update();
+							$i_form->js(null,$i_form->js()->reload())->univ()->successMessage('Update Information')->execute();
+						}
+						$i_form->addClass('panel panel-default');
+						$i_form->addStyle('padding','20px');
+
 	}
 
 }
