8745f88e1986bd85d110056b04a676bb4dad165f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-04-10 17:14:53 +0530

    Lead View Layout Beutify

--- epan-components/xMarketingCampaign/lib/Model/Lead.php
+++ epan-components/xMarketingCampaign/lib/Model/Lead.php
@@ -21,17 +21,17 @@ class Model_Lead extends \Model_Document{
 		$this->hasOne('Epan','epan_id');
 		$this->addCondition('epan_id',$this->api->current_website->id);
 
-		$this->addField('name')->sortable(true);
-		$this->addField('organization_name')->sortable(true);
-		$this->addField('email_id')->sortable(true);
+		$this->addField('lead_type')->enum(array('Sales'))->group('a~2~Basic Information');
+		$this->addField('name')->sortable(true)->group('a~3');
+		$this->addField('organization_name')->sortable(true)->group('a~4');
+		$this->addField('website')->sortable(true)->group('a~3');
 		// $this->addField('status');
 		$this->addField('source')->sortable(true);
 		$this->addField('source_id')->sortable(true);
-		$this->addField('phone')->sortable(true);
-		$this->addField('mobile_no')->sortable(true);
-		$this->addField('fax');
-		$this->addField('website')->sortable(true);
-		$this->addField('lead_type')->enum(array('Sales'));
+		$this->addField('phone')->sortable(true)->group('b~3~Contact Information');
+		$this->addField('mobile_no')->sortable(true)->group('b~3');
+		$this->addField('email_id')->sortable(true)->group('b~3');
+		$this->addField('fax')->group('b~3');
 
 		$this->hasMany('xShop/Model_Oppertunity','lead_id');
 		$this->hasMany('xShop/Model_Quotation','lead_id');
