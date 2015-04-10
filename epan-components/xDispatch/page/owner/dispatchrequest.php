9cf619d0d249a526e33ca37d954bb6b21e2de8fb
sinharakesh <rksinha.btech@gmail.com>
2015-04-10 20:23:59 +0530

    link add

--- epan-components/xDispatch/page/owner/dispatchrequest.php
+++ epan-components/xDispatch/page/owner/dispatchrequest.php
@@ -10,6 +10,7 @@ class page_xDispatch_page_owner_dispatchrequest extends page_xDispatch_page_owne
 		$tabs->addTabURL('xDispatch_page_owner_dispatchrequest_received','Received');
 		$tabs->addTabURL('xDispatch_page_owner_dispatchrequest_partialcomplete','Partial Complete');
 		$tabs->addTabURL('xDispatch_page_owner_dispatchrequest_complete','Complete');
+		$tabs->addTabURL('xDispatch_page_owner_dispatchrequest_cancel','Cancel');
 	}
 
 }
