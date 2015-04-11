4b6f7bb6ef3dd49a968b1451d373b63f1f0c5313
sinharakesh <rksinha.btech@gmail.com>
2015-04-11 14:31:40 +0530

    beforesave

--- epan-components/xShop/lib/Grid/Quotation.php
+++ epan-components/xShop/lib/Grid/Quotation.php
@@ -17,32 +17,13 @@ class Grid_Quotation extends \Grid{
 		$this->current_row_html[$field] = '<a href="#na" onclick="javascript:'.$this->js()->univ()->frameURL('Quotation', $this->api->url($this->vp->getURL(),array('quotation_clicked'=>$this->model->id))).'">'. $this->current_row[$field] ."</a>";
 	}
 
-
-
-	// 	$this->addQuickSearch(array('name','status','quotation_no'));
-	// 	$this->addPaginator($ipp=50);
-	// }
-
-	// function setModel($m){
-	// 	parent::setModel($m);
-
-	// 	$self= $this;
-	// 	$vp = $this->add('VirtualPage')->set(function($p)use($self){
-	// 		$p->add('xShop/View_Quotation',array('quotation'=>$self->add('xShop/Model_Quotation')->load($_GET['quotation_clicked'])));
-	// 	});
-
-	// 	$this->js(true)->find('tr')->css('cursor','pointer');
-	// 	$this->on('click','tbody td:not(:has(button))',$this->js()->univ()->frameURL('Quotation',array($this->api->url($vp->getURL()),'quotation_clicked'=>$this->js()->_selectorThis()->closest('tr')->data('id'))));
-		
-		// $this->addFormatter('edit','myedit');
-
 	function format_myedit($field){
 		if($this->model->status() == 'approved')
 			$this->current_row_html[$field]='';
 	}
 	
-	function setModel($job_card_model){
-		$m=parent::setModel($job_card_model,array('name','created_at'));
+	function setModel($quotation_model){
+		$m=parent::setModel($quotation_model);
 		$this->addFormatter('name','view');
 		$this->addColumn('expander','items',array('page'=>'xShop_page_owner_quotation_items','descr'=>'Items'));
 
