567219795535700aef6f8bc5ca5796e913e47453
sinharakesh <rksinha.btech@gmail.com>
2015-04-12 00:26:25 +0530

    Quotaion Item Updated

--- epan-components/xShop/lib/View/Quotation.php
+++ epan-components/xShop/lib/View/Quotation.php
@@ -11,8 +11,8 @@ class View_Quotation extends \CompleteLister{
 		$this->template->setHtml('name',"Quotation No: <b>".$this->quotation['name']."</b>");
 		$this->template->setHtml('created_at',"Created At: <b>".$this->quotation['created_at']."</b>");
 		$this->template->setHtml('status',"Status: <b>".ucwords($this->quotation['status'])."</b>");
-		$this->template->setHtml('customer',"Customer: <b>".ucwords($this->quotation['customer'])."</b>");
-		$this->template->setHtml('lead',"Lead: <b>".ucwords($this->quotation['lead'])."<b>");
+		$this->quotation['customer']?$this->template->setHtml('customer',"Customer: <b>".ucwords($this->quotation['customer'])."</b>"):"";
+		$this->quotation['lead']?$this->template->setHtml('lead',"Lead: <b>".ucwords($this->quotation['lead'])."<b>"):"";
 		
 		$this->setModel($this->quotation->itemrows());
 	}
