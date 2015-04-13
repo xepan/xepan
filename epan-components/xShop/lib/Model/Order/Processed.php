3ddfe2499780c91fa1048901f550a783f3956340
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-04-13 10:14:14 +0530

    Sales Dashboard

--- epan-components/xShop/lib/Model/Order/Processed.php
+++ epan-components/xShop/lib/Model/Order/Processed.php
@@ -9,11 +9,26 @@ class Model_Order_Processed extends Model_Order{
 			'allow_edit'=>array(),
 			'allow_add'=>array(),
 			'allow_add'=>array(),
+			'can_mark_processed'=>array(),
 		);
 	
 	function init(){
 		parent::init();
 
 		$this->addCondition('status','processed');
+	}
+
+	function can_mark_processed_page($p){
+		$form = $p->add('Form');
+		$form->addSubmit('Complete This Order');
+
+		if($form->isSubmitted()){
+			$this->can_mark_processed();
+			return true;
+		}
+	}
+
+	function can_mark_processed(){
+		$this->setStatus('completed');
 	}
 }
--- epan-components/xShop/page/owner/dashboard.php
+++ epan-components/xShop/page/owner/dashboard.php
@@ -7,17 +7,47 @@ class page_xShop_page_owner_dashboard extends page_xShop_page_owner_main{
 
 		$this->app->title=$this->api->current_department['name'] .': Dashboard';
 
+		// TOTAL WORKING PROJECTS
+		$running_orders = $this->add('xShop/Model_Order')->addCondition('status',array('approved','processing','processed'));
+		$t_app_order = $this->add('View_Info')->setHtml('Running Works:'.$running_orders->count()->getOne() ." Amount :".$running_orders->sum('net_amount'));
+		
 		//TODAY APPROVE ORDERS WITH AMOUNTS
-		$submit_order = $this->add('xShop/Model_Order_Submitted')->addCondition('created_date',$this->api->today);
-		$t_app_order = $this->add('View_Info')->setHtml('Today Approved Orderd:'.$submit_order->MyCounts(true,false)." Amount :".$submit_order->sum('net_amount'));
+		$approved_order = $this->add('xShop/Model_Order');
+		$approved_order->addExpression('approved_on')->set(function($m,$q){
+			$act = $m->add('xCRM/Model_Activity')
+				->addCondition('action','approved')
+				->addCondition('related_root_document_name',$m->root_document_name)
+				->addCondition('related_document_id',$q->getField('id'))
+				->setOrder('updated_at','desc')
+				->setLimit(1);
+			return $act->fieldQuery('created_at');
+		});
+
+		$approved_order->addCondition('approved_on','>',$this->api->today);
+		$approved_order->addCondition('approved_on','<=',$this->api->nextDate($this->api->today));
+
+		$t_app_order = $this->add('View_Info')->setHtml('Today Approved Orderd:'.$approved_order->count()->getOne() ." Amount :".$approved_order->sum('net_amount'));
 
 		//TODAY Complete ORDERS WITH AMOUNTS
-		$complete_order = $this->add('xShop/Model_Order_Completed')->addCondition('created_date',$this->api->today);		
-		$this->add('View_Info')->setHtml('Today Complete Orderd:'.$complete_order->MyCounts(true,false)." Amount :".$complete_order->sum('net_amount'));
+		$complete_order = $this->add('xShop/Model_Order');
+		$complete_order->addExpression('complete_on')->set(function($m,$q){
+			$act = $m->add('xCRM/Model_Activity')
+				->addCondition('action','completed')
+				->addCondition('related_root_document_name',$m->root_document_name)
+				->addCondition('related_document_id',$q->getField('id'))
+				->setOrder('updated_at','desc')
+				->setLimit(1);
+			return $act->fieldQuery('created_at');
+		});
+
+		$complete_order->addCondition('complete_on','>',$this->api->today);
+		$complete_order->addCondition('complete_on','<=',$this->api->nextDate($this->api->today));
+
+		$this->add('View_Info')->setHtml('Today Complete Orders:'.$complete_order->count()->getOne()." Amount :".$complete_order->sum('net_amount'));
 
 		//TODAY CANCEL ORDERS WITH AMOUNTS
-		$cancel_order = $this->add('xShop/Model_Order_Cancelled')->addCondition('created_date',$this->api->today);		
-		$this->add('View_Info')->setHtml('Today Canceled Orderd:'.$cancel_order->MyCounts(true,false)." Amount :".$cancel_order->sum('net_amount'));
+		$cancel_order = $this->add('xShop/Model_Order_Cancelled')->addCondition('updated_date',$this->api->today);		
+		$this->add('View_Info')->setHtml('Today Canceled Orders:'.$cancel_order->MyCounts(true,false)." Amount :".$cancel_order->sum('net_amount'));
 
 
 		//DEPARTMENT WISE JOBCARD
@@ -44,6 +74,27 @@ class page_xShop_page_owner_dashboard extends page_xShop_page_owner_main{
 		}
 
 		$dept_jobcard_v->setHtml($str);
+
+		$commited_order = $this->add('xShop/Model_Order')->addCondition('status',array('approved','processing','processed'));
+		$commited_order->addCondition('delivery_date','>=',$this->api->today);
+		$commited_order->addCondition('delivery_date','<=',$this->api->nextDate($this->api->nextDate($this->api->today)));
+		$commited_order->setOrder('delivery_date','desc');
+
+		$t_app_order = $this->add('View_Info')->setHtml('Commitments (Today+Tomorrow):'.$commited_order->count()->getOne() ." Amount :".$commited_order->sum('net_amount'));
+		$crud= $this->add('CRUD',array('grid_class'=>'xShop/Grid_Order'));
+		$crud->grid->ipp=5;
+		$crud->setModel($commited_order);
+		$crud->add('xHR/Controller_Acl',array('override'=>array('can_view'=>"All",'allow_edit'=>'No','can_forceDelete'=>'No')));
+
+		$commited_order = $this->add('xShop/Model_Order')->addCondition('status',array('approved','processing','processed'));
+		$commited_order->addCondition('delivery_date','<',$this->api->today);
+		$commited_order->setOrder('delivery_date','desc');
+
+		$t_app_order = $this->add('View_Info')->setHtml('Over Due Orders:'.$commited_order->count()->getOne() ." Amount :".$commited_order->sum('net_amount'));
+		$crud= $this->add('CRUD',array('grid_class'=>'xShop/Grid_Order'));
+		$crud->grid->ipp=5;
+		$crud->setModel($commited_order);
+		$crud->add('xHR/Controller_Acl',array('override'=>array('can_view'=>"All",'allow_edit'=>'No','can_forceDelete'=>'No')));
 
 	}
 }		
