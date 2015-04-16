9119b20b2f41eb50d711c3944070d1c3b92718ca
xavocvijay <guddavijay@gmail.com>
2015-04-16 10:19:02 +0530

    Tasks resotered

--- epan-components/xProduction/page/owner/task/processing.php
+++ epan-components/xProduction/page/owner/task/processing.php
@@ -5,12 +5,24 @@ class page_xProduction_page_owner_task_processing extends page_xProduction_page_
 	function init(){
 		parent::init();
 
-		$task = $this->add('xProduction/Model_Task_Processing');
+		$col=$this->add('Columns');
+		$left_col=$col->addColumn(6);
+		$right_col=$col->addColumn(6);
+		$left_col->add('View_Info')->set('Processing By Me');
+		$mytask = $left_col->add('xProduction/Model_Task_Processing');
+		$mytask->addCondition('employee_id',$this->api->current_employee->id);
 		
-		$crud=$this->add('CRUD');
-		$crud->setModel($task);
+		$crud=$left_col->add('CRUD');
+		$crud->setModel($mytask);
 
 		$crud->add('xHR/Controller_Acl');
-		
+
+		$right_col->add('View_Info')->set('Processing By Employee');
+		$emptask = $right_col->add('xProduction/Model_Task_Processing');
+		$emptask->addCondition('created_by_id',$this->api->current_employee->id);
+		$crud=$right_col->add('CRUD',array('grid_class'=>'xProduction/Grid_Task'));
+		$crud->setModel($emptask);
+
+		$crud->add('xHR/Controller_Acl');
 	}
 }
--- epan-components/xProduction/page/owner/user/tasks.php
+++ epan-components/xProduction/page/owner/user/tasks.php
@@ -6,11 +6,12 @@ class page_xProduction_page_owner_user_tasks extends page_xProduction_page_owner
 		parent::init();
 
 		$tabs=$this->app->layout->add('Tabs');
-		$tabs->addTabURL('xProduction/page/owner/task_assigned','Assigned to me'.$this->add('xProduction/Model_Task_Assigned')->myUnRead());
-		$tabs->addTabURL('xProduction/page/owner/task_processing','Processing'.$this->add('xProduction/Model_Task_Processing')->myUnRead());
-		$tabs->addTabURL('xProduction/page/owner/task_processed','Processed '.$this->add('xProduction/Model_Task_Processed')->myUnRead());
-		$tabs->addTabURL('xProduction/page/owner/task_completed','Completed By Me '.$this->add('xProduction/Model_Task_Completed')->myUnRead());
-		$tabs->addTabURL('xProduction/page/owner/task_cancelled','Cancelled '.$this->add('xProduction/Model_Task_Cancelled')->myUnRead());
+		$tabs->addTabURL('xProduction/page/owner/task_draft','Draft'.$this->add('xProduction/Model_Task_Draft')->myCounts(true,false));
+		$tabs->addTabURL('xProduction/page/owner/task_assigned','Assigned'.$this->add('xProduction/Model_Task_Assigned')->myCounts(true,false));
+		$tabs->addTabURL('xProduction/page/owner/task_processing','Processing'.$this->add('xProduction/Model_Task_Processing')->myCounts(true,false));
+		$tabs->addTabURL('xProduction/page/owner/task_processed','Processed '.$this->add('xProduction/Model_Task_Processed')->myCounts(true,false));
+		$tabs->addTabURL('xProduction/page/owner/task_completed','Completed By Me '.$this->add('xProduction/Model_Task_Completed')->myCounts(true,false));
+		$tabs->addTabURL('xProduction/page/owner/task_cancelled','Cancelled '.$this->add('xProduction/Model_Task_Cancelled')->myCounts(true,false));
 
 		
 	}
