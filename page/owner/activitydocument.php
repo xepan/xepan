82710dc5ac5bc4850a9ae8aa17d589c631cec30b
sinharakesh <rksinha.btech@gmail.com>
2015-04-17 10:28:38 +0530

    name changed to activities

--- page/owner/activitydocument.php
+++ page/owner/activitydocument.php
@@ -9,27 +9,37 @@ class page_owner_activitydocument extends page_base_owner{
 						'xPurchase\PurchaseOrder'=>array(
 							'view'=>'xPurchase\View_PurchaseOrder',
 							'model'=>'xPurchase\Model_PurchaseOrder',
-							'mobel_variable'=>'purchaseorder'
+							'model_variable'=>'purchaseorder'
 							),
 						'xShop\Order'=>array(
 							'view'=>'xShop\View_Order',
 							'model'=>'xShop\Model_Order',
-							'mobel_variable'=>'order'
+							'model_variable'=>'order'
 							),
 						'xProduction\JobCard'=>array(
 							'view'=>'xProduction\View_Jobcard',
 							'model'=>'xProduction\Model_JobCard',
-							'mobel_variable'=>'jobcard'
+							'model_variable'=>'jobcard'
 							),
 						'xStore\StockMovement'=>array(
 							'view'=>'xStore\View_StockMovement',
 							'model'=>'xStore\Model_StockMovement',
-							'mobel_variable'=>'stockmovement'
+							'model_variable'=>'stockmovement'
 							),
 						'xDispatch\DispatchRequest'=>array(
 							'view'=>'xDispatch\View_DispatchRequest',
 							'model'=>'xDispatch\Model_DispatchRequest',
-							'mobel_variable'=>'dispatchrequest'
+							'model_variable'=>'dispatchrequest'
+							),
+						'xShop\Invoice'=>array(
+							'view'=>'View_SalesInvoice',
+							'model'=>'xShop\Model_SalesInvoice',
+							'model_variable'=>'invoice'
+							),
+						'xPurchase\PurchaseInvoice'=>array(
+							'view'=>'View_PurchaseInvoice',
+							'model'=>'xPurchase\Model_PurchaseInvoice',
+							'model_variable'=>'invoice'
 							)
 					);
 
@@ -45,7 +55,7 @@ class page_owner_activitydocument extends page_base_owner{
 			$selected_view_array = $view_array[$activity['related_root_document_name']];
 			$selected_view = $selected_view_array['view'];
 			$selected_model = $selected_view_array['model'];
-			$this->add($selected_view,array($selected_view['mobel_variable']=>$this->add($selected_model)->load($activity['related_document_id'])));
+			$this->add($selected_view,array($selected_view_array['model_variable']=>$this->add($selected_model)->load($activity['related_document_id'])));
 			// $this->add();
 
 		}
