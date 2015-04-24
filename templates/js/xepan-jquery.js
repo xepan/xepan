356b5db7004af4641d3a8d2bbea3033f62115c8a
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-04-24 18:16:00 +0530

    tooltip Html patch for jquery

--- templates/js/xepan-jquery.js
+++ templates/js/xepan-jquery.js
@@ -50,3 +50,12 @@
     }
     
 })(jQuery);
+
+$.widget("ui.xtooltip", $.ui.tooltip, {
+    options: {
+        content: function () {
+            return $(this).prop('title');
+        },
+        position: { my: "center top+15", at: "center center" }
+    }
+});
