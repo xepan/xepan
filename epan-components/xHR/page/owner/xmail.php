ec0cebbbdf4024d4a1fca30acd6435595a8862ac
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-04 12:33:15 +0530

    email fetch according to customer and supplier

--- epan-components/xHR/page/owner/xmail.php
+++ epan-components/xHR/page/owner/xmail.php
@@ -16,6 +16,7 @@ class page_xHR_page_owner_xmail extends page_xHR_page_owner_main{
 
 		$col=$this->add('Columns');
 		$left_col=$col->addColumn(3);
+		$right_col=$col->addColumn(9);
 
 		$customer=$this->add('xShop/Model_Customer');
 		$customer->addExpression('unread')->set(function($m,$q){
@@ -39,6 +40,14 @@ class page_xHR_page_owner_xmail extends page_xHR_page_owner_main{
 
 		$customer_crud=$left_col->add('CRUD',array('grid_class'=>'xHR/Grid_MailParty','allow_add'=>false,'allow_edit'=>false,'allow_del'=>false));
 		$customer_crud->setModel($customer);
+		
+		if(!$customer_crud->isEditing()){
+			$customer_crud->grid->addMethod('format_anchor',function($g,$f)use($right_col){
+					$g->current_row_html[$f]='<a href="javascript:void(0)" onclick="'.$right_col->js()->reload(array('customer_id'=>$g->model->id)).'">'.$g->model['customer_name'].' ( '.$g->model['unread'].' ) '.'</a>';
+				});
+			$customer_crud->grid->addFormatter('customer_name','anchor');
+		}
+
 
 		$supplier=$this->add('xPurchase/Model_Supplier');
 		$supplier->addExpression('unread')->set(function($m,$q){
@@ -63,8 +72,15 @@ class page_xHR_page_owner_xmail extends page_xHR_page_owner_main{
 
 		$supplier_crud=$left_col->add('CRUD',array('grid_class'=>'xHR/Grid_MailParty','allow_add'=>false,'allow_edit'=>false,'allow_del'=>false));
 		$supplier_crud->setModel($supplier);
+
+		if(!$supplier_crud->isEditing()){
+			$supplier_crud->grid->addMethod('format_anchor',function($g,$f)use($right_col){
+					$g->current_row_html[$f]='<a href="javascript:void(0)" onclick="'.$right_col->js()->reload(array('supplier_id'=>$g->model->id)).'">'.$g->model['name'].' ( '.$g->model['unread'].' ) '.'</a>';
+				});
+			$supplier_crud->grid->addFormatter('name','anchor');
+		}
 		
-		$right_col=$col->addColumn(9);
+
 
 		$email = $this->add('xCRM/Model_Email');
 		$emails = $email->loadDepartmentEmails();
@@ -74,16 +90,71 @@ class page_xHR_page_owner_xmail extends page_xHR_page_owner_main{
 		// $official_email = $this->add('xHR/Model_OfficialEmail');
 		// $official_email->addCondition('department_id',$dept_id);
 
+		//SHOW ONLY CUSTOMER EMAILS
+		if($_GET['customer_id']){
+
+			$emails->addCondition(
+				$emails->dsql()->orExpr()
+							->where(
+									$emails->dsql()->andExpr()
+									->where('from','Customer')
+									->where('from_id',$_GET['customer_id'])
+								)
+							->where(
+									$emails->dsql()->andExpr()
+										->where('to','Customer')
+										->where('to_id',$_GET['customer_id'])
+								)
+					);
+
+		}
+
+		if($_GET['supplier_id']){
+
+			$emails->addCondition(
+				$emails->dsql()->orExpr()
+							->where(
+									$emails->dsql()->andExpr()
+									->where('from','Supplier')
+									->where('from_id',$_GET['supplier_id'])
+								)
+							->where(
+									$emails->dsql()->andExpr()
+										->where('to','Supplier')
+										->where('to_id',$_GET['supplier_id'])
+								)
+					);			
+		}
 
 		$mail_crud=$right_col->add('CRUD');
-		$mail_crud->setModel($emails,array(),array('subject'));
+		$mail_crud->setModel($emails,array(),array('subject','to_email','from_email'));
 		$mg=$mail_crud->grid;
 		
-		$mg->addMethod('format_subject',function($g,$f)use($message_vp){
-			$g->current_row_html[$f]='<a href="javascript:void(0)" onclick="'.$g->js()->univ()->frameURL('E-mail',$g->api->url($message_vp->getURL(),array('xcrm_email_id'=>$g->model->id))).'">'.$g->current_row[$f].'</a>';
-		});
-		$mg->addFormatter('subject','subject');
+		if(!$mail_crud->isEditing()){
+			$mg->addMethod('format_subject',function($g,$f)use($message_vp){
+				//Check for Email is Incomening or OutGoing
+				$snr = "";									
+				if($g->model->isReceive()){	
+					$snr = '<small class="atk-swatch-green">In</small>';
+				}elseif($g->model->isSent()){
+					$snr = '<small class="atk-swatch-yellow">Out</small>';
+				}
 
+				$str = '<div  class="atk-row">';
+				$str.= '<div class="atk-col-5">'.$g->model['from_email'].'</div>';
+				$str.= '<small class="atk-col-2">'.$snr.'</small>';
+				$str.= '<div class="atk-col-5">'.$g->model['to_email'].'</div>';
+				$str.= '</div>';
+
+				$g->current_row_html[$f] = '<a href="javascript:void(0)" onclick="'.$g->js()->univ()->frameURL('E-mail',$g->api->url($message_vp->getURL(),array('xcrm_email_id'=>$g->model->id))).'">'.$g->current_row[$f].'</a>'.$str;
+			});
+			$mg->addFormatter('subject','subject');
+			
+			$mg->removeColumn('to_email');
+			$mg->removeColumn('from_email');
+		}
+
+		
 
 		$mail_crud->add('xHR/Controller_Acl');
 
@@ -92,7 +163,6 @@ class page_xHR_page_owner_xmail extends page_xHR_page_owner_main{
 			$this->add('xCRM/Model_Email')->fetchDepartment($this->api->current_department);
 			$this->js()->univ()->successMessage('Fetch Successfully')->execute();
 		}
-
 
 	}
 }
