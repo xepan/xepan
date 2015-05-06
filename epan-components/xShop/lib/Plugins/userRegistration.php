8e2edc3eb6cdbff8763ea8698ebc317a21a716dc
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-06 17:15:22 +0530

    Event catched for user deleted

--- epan-components/xShop/lib/Plugins/userRegistration.php
+++ epan-components/xShop/lib/Plugins/userRegistration.php
@@ -9,6 +9,7 @@ class Plugins_userRegistration extends \componentBase\Plugin{
 		parent::init();
 
 		$this->addHook('new_user_registered',array($this,'new_user_registered'));
+		$this->addHook('user_before_delete',array($this,'user_before_delete'));
 	}
 
 	function new_user_registered($obj,$user_model){
@@ -16,6 +17,14 @@ class Plugins_userRegistration extends \componentBase\Plugin{
 		$member_detail_model = $this->add('xShop/Model_MemberDetails');
 		$member_detail_model['users_id'] = $user_model['id'];
 		$member_detail_model->save();
+	}
+
+	function user_before_delete($user_to_be_deleted){
+		$this->add('xShop/Model_MemberDetails')
+			->addCondition('users_id',$user_to_be_deleted->id)
+			->_dsql()
+			->set('users_id',null)
+			->update();
 	}
 
 }
