36b238b54765a9b77469e6b202471d11c0f989ee
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-08 16:44:26 +0530

    Test Base Updated

--- page/tests/base.php
+++ page/tests/base.php
@@ -1,7 +1,8 @@
 <?php
 
 class page_tests_base extends Page_Tester {
-	
+	public $proper_responses=array('Test_empty'=>'');
+
 	function init(){
 		ini_set('memory_limit', '2048M');
 		set_time_limit(0);		
