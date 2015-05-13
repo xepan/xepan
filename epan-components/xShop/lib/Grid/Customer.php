2b396cf88bd73f71c8ee956427f40d1e237be898
xavocvijay <guddavijay@gmail.com>
2015-05-13 11:41:40 +0530

    Customer Grid Modified

--- epan-components/xShop/lib/Grid/Customer.php
+++ epan-components/xShop/lib/Grid/Customer.php
@@ -19,11 +19,26 @@ class Grid_Customer extends \Grid{
 										'country','pincode'));
 
 		if($this->hasColumn('item_name')) $this->removeColumn('item_name');
-		// $this->fooHideAlways('username');
+
+		$this->addFormatter('customer_name','wrap');
+		$this->addFormatter('address','wrap');
+		$this->addFormatter('customer_email','wrap');
+		$this->addFormatter('other_emails','wrap');
+		$this->addFormatter('mobile_number','wrap');
+		
+		$this->fooHideAlways('state');
+		$this->fooHideAlways('country');
+		$this->fooHideAlways('billing_address');
+		$this->fooHideAlways('shipping_address');
 
 		$this->addPaginator(15);
 		$this->addQuickSearch(array('name','no_person','discount_amount','from','to'));
 		$this->add_sno();
 		return $m;	
+	}
+
+	function formatRow(){
+		$this->current_row_html['customer_name']=$this->model['customer_name'];
+		parent::formatRow();
 	}
 }
