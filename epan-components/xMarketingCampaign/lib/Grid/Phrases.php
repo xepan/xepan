d82b8e6f6048b34c462d3d38ff4c9a73ce968a1d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-14 13:04:50 +0530

    Modified Phrases Grid

--- epan-components/xMarketingCampaign/lib/Grid/Phrases.php
+++ epan-components/xMarketingCampaign/lib/Grid/Phrases.php
@@ -3,14 +3,17 @@ namespace xMarketingCampaign;
 class Grid_Phrases extends \Grid{
 	function init(){
 		parent::init();
-		$this->addPaginator(10);;
-		$this->add_sno();
 	}
 	function setModel($model){
 		$m = parent::setModel($model,array('subscription_category','name','is_grabbed','last_page_checked_at','emails_count'));
 	
 		$this->fooHideAlways('last_run_time');
 		$this->fooHideAlways('site_url');
+		
+		$this->addQuickSearch(array('name','subscription_category'),null,'xMarketingCampaign/Filter_Phrases');
+		$this->addPaginator(10);;
+		$this->add_sno();
+		
 		return $m;
 	}
 	function formatRow(){
