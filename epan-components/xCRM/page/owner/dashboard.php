3d74b2cf639695aa81cfdc83519a90bbdb6273a5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-15 13:28:48 +0530

    Modified & Correct Page Tittle

--- epan-components/xCRM/page/owner/dashboard.php
+++ epan-components/xCRM/page/owner/dashboard.php
@@ -2,7 +2,8 @@
 
 class page_xCRM_page_owner_dashboard extends page_xCRM_page_owner_main {
 	function initMainPage(){
-		//$this->add('H1')->set('Component Owner Main Page');
+		$this->app->title=$this->api->current_department['name'] .': Dashboard';
+		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-users"></i> xCRM Dashboard <small> Manage your Customer   </small>');
 	}
 
 
--- epan-components/xCRM/page/owner/ticket.php
+++ epan-components/xCRM/page/owner/ticket.php
@@ -3,6 +3,7 @@
 class page_xCRM_page_owner_ticket extends page_componentBase_page_owner_main {
 	function init(){
 		parent::init();
+		$this->app->title=$this->api->current_department['name'] .': Support Ticket';
 		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-users"></i> Ticket Management <small> Manage your customer complains </small>');
 
 		$tab = $this->add('Tabs');
