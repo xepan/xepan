12ced3d4bc346f76ddf473ce360ec8647e4216ee
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-16 12:29:37 +0530

    Search Tool Options add

--- epan-components/slideShows/page/owner/main.php
+++ epan-components/slideShows/page/owner/main.php
@@ -3,7 +3,7 @@
 class page_slideShows_page_owner_main extends page_componentBase_page_owner_main {
 	function page_index(){
 		// $this->add('H1')->set('Component Owner Main Page');
-		$this->h1->setHTML('<i class="fa fa-film"></i> '.$this->component_name. '<small>Attractive, Custom and Effectfull Slide Shows</small>');
+		$this->add('H1')->setHTML('<i class="fa fa-film"></i> '.$this->component_name. '<small>Attractive, Custom and Effectfull Slide Shows</small>');
 		$tab=$this->add('Tabs');
 		$gallery_tab=$tab->addTabURL('slideShows/page_owner_awesomeslider','Add Awesome Slider Gallery');
 		$gallery_tab=$tab->addTabURL('slideShows/page_owner_thumbnailslider','Add Thumbanil Slider Images');
