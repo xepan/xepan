00b1a8072837c573e55921fa31145a6aa2bc5139
sinharakesh <rksinha.btech@gmail.com>
2015-05-18 12:24:36 +0530

    xShop Category Grid Modified

--- epan-components/xShop/lib/Grid/Category.php
+++ epan-components/xShop/lib/Grid/Category.php
@@ -6,9 +6,30 @@ class Grid_Category extends \Grid{
 	function init(){
 		parent::init();
 		
+
+	}
+
+	function setModel($model){
+		$m = parent::setModel($model,array('name','parent','order_no','is_active'));
+		
+		if($this->hasColumn('is_active')) $this->removeColumn('is_active');
+		
+		$this->addFormatter('name','wrap');
 		$this->add_sno();
 		$this->addQuickSearch(array('name','parent'));
 		$this->addPaginator($ipp=100);
+		return $m;
+	}
+
+	function formatRow(){
+		
+		if(!$this->model['is_active']){
+			$this->setTDParam('name','style/color','red');
+		}else{
+			$this->setTDParam('name','style/color','');
+		}
+		parent::formatRow();
+
 	}
 
 	function recursiveRender(){
