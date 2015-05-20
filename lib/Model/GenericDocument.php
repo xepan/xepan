415778588e5973cca9d6493bba6ec3750d7621cd
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-20 22:12:53 +0530

    Generic Documents page

--- lib/Model/GenericDocument.php
+++ lib/Model/GenericDocument.php
@@ -2,12 +2,21 @@
 
 class Model_GenericDocument extends Model_Document {
 	public $table='xepan_generic_documents';
-	
+
 	public $root_document_name = "\GenericDocument";
 	public $status=array('draft','submitted','inactive');
 
+	public $actions=array(
+		'allow_add'=>array(),
+		'allow_edit'=>array(),
+		'allow_del'=>array()
+		);
+
 	function init(){
 		parent::init();
+
+		$this->hasOne('Epan','epan_id');
+		$this->addCondition('epan_id',$this->api->current_website->id);
 
 		$this->addField('title');
 		$this->addField('content')->type('text')->display(array('form'=>'RichText'));
