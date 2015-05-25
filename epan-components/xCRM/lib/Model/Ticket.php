43488d348154de1ee293fe52972c2e1f035ba8f6
sinharakesh <rksinha.btech@gmail.com>
2015-05-25 12:59:41 +0530

    official email auto reply email filed added

--- epan-components/xCRM/lib/Model/Ticket.php
+++ epan-components/xCRM/lib/Model/Ticket.php
@@ -17,6 +17,23 @@ class Model_Ticket extends \Model_Document{
  		$this->hasOne('xShop/Model_Order','order_id');
 		
 		$this->addField('name');
+		$this->addField('uid');
+		$this->addField('from'); // Customer, Employee, Supplier ... string
+		$this->addField('from_id');
+		$this->addField('from_email');
+		$this->addField('from_name');
+
+		$this->addField('to');
+		$this->addField('to_id');
+		$this->addField('to_email');
+
+		$this->addField('cc')->type('text');
+		$this->addField('bcc')->type('text');
+
+		$this->addField('subject');
+		$this->addField('message')->type('text');
+
+		$this->addField('priority')->enum(array('Low','Medium','High','Urgent'))->defaultValue('Medium');
 
 		$this->hasMany('xCRM/TicketAttachment','related_document_id',null,'Attachments');
 		//$this->add('dynamic_model/Controller_AutoCreator');	
@@ -27,4 +44,40 @@ class Model_Ticket extends \Model_Document{
 	// 	$this['name'] = "[".$this->root_document_name.$this['name']."]";
 	// }
 
+	function supportEmail(){
+		$off_email = $this->add('xHR/Model_OfficialEmail');
+		$off_email->addCondition('imap_email_username',$this['to_email'])
+					->addCondition('status','active');
+		return $off_email->tryLoadAny();
+	}
+
+	function autoReply(){
+		if(!$this->loaded()) return false;
+
+		if($this->supportEmail()->count()->getOne()){
+			$support_email = $this->supportEmail();
+			if(!$support_email['auto_reply']) return false;
+			
+			if(!$this['from_email'])
+				return false;
+
+			$subject = $support_email['email_subject']?:"Ticket Created";
+			$subject = str_replace("{{customer_name}}", $this['customer'], $subject);
+			$subject = str_replace("{{ticket_number}}", $this['name'], $subject);
+			
+			$email_body = $support_email['email_body'];
+			$footer = $support_email['footer'];
+
+			$email_body = str_replace("{{ticket_number}}", $this['name'], $email_body);
+			$email_body = str_replace("{{status}}", $this['status'], $email_body);
+			$email_body = str_replace("{{priority}}", $this['priority'], $email_body);
+			$email_body = str_replace("{{customer_name}}", $this['customer']?$this['customer']:" ", $email_body);
+			$email_body = str_replace("{{created_date}}", $this['created_date'], $email_body);
+
+			$this->sendEmail($this['from_email'],$subject,$email_body,$this['cc'],$this['bcc']);
+				
+			$email_to = $this['from_email'].','.$this['cc'].$this['bcc'];
+			$new_activity = $this->createActivity('Email',$subject,$email_body,$this['from'],$this['from_id'], $this['to'], $this['to_id'],$email_to);
+		}
+	}
 }
