255cdcd732ba51f6b9cc32d4249f0953554e4618
sinharakesh <rksinha.btech@gmail.com>
2015-05-26 14:09:22 +0530

    Fields number added

--- epan-components/xShop/page/owner/shopsnblogs.php
+++ epan-components/xShop/page/owner/shopsnblogs.php
@@ -146,6 +146,17 @@ class page_xShop_page_owner_shopsnblogs extends page_xShop_page_owner_main {
 						}
 						$cash_form->addClass('panel panel-default');
 						$cash_form->addStyle('padding','20px');	
+
+			$number_tab=$tab->addTab('Starting Number');
+			$number_form = $number_tab->add('Form');
+			$number_form->setModel($config_model,array('quotation_starting_number','sale_order_starting_number','purchase_order_starting_number','sale_invoice_starting_number','purchase_invoice_starting_number'));
+			$number_form->addSubmit('Update');
+			if($number_form->Submitted()){
+							$number_form->Update();
+							$number_form->js(null,$number_form->js()->reload())->univ()->successMessage('Updated')->execute();
+						}
+			$number_form->addClass('panel panel-default');
+			$number_form->addStyle('padding','20px');
 	}
 
 }
