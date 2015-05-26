3b48b0ce7cd6be8e3dc933b16ddc0a7f03547b94
sinharakesh <rksinha.btech@gmail.com>
2015-05-26 13:36:24 +0530

    tooltip add

--- epan-components/xHR/lib/Grid/OfficialEmail.php
+++ epan-components/xHR/lib/Grid/OfficialEmail.php
@@ -19,7 +19,8 @@ class Grid_OfficialEmail extends \Grid{
 						'imap_email_port',
 						'imap_email_username',
 						'imap_email_password',
-						'imap_flags'
+						'imap_flags',
+						'is_support_email'
 					));
 		// $this->addColumn('IMAP');
 	}
@@ -41,7 +42,9 @@ class Grid_OfficialEmail extends \Grid{
 						'denied_email_subject',
 						'denied_email_body',
 						'email_body',
-						'email_subject'
+						'email_subject',
+						'auto_reply',
+						'is_support_email',
 					);
 		}
 
@@ -63,6 +66,8 @@ class Grid_OfficialEmail extends \Grid{
 		if($this->hasColumn('denied_email_subject'))$this->removeColumn('denied_email_subject');
 		if($this->hasColumn('email_body'))$this->removeColumn('email_body');
 		if($this->hasColumn('email_subject'))$this->removeColumn('email_subject');
+		if($this->hasColumn('auto_reply'))$this->removeColumn('auto_reply');
+		if($this->hasColumn('is_support_email'))$this->removeColumn('is_support_email');
 		// $this->addFormatter('name','name11');
 		return $m;
 	}
@@ -74,7 +79,24 @@ class Grid_OfficialEmail extends \Grid{
 		if($this->model['status']!="active")
 			$status = "atk-swatch-red";
 		
-		$str = '<div class="text-center atk-size-giga '.$status.' "> '.$this->model['email_username'].'</div>';
+		if($this->model['is_support_email']){
+			$support_html = '<span class="icon-comment" title="This is Support Email"></span>';
+		}else{
+			$support_html = '<span class="icon-comment atk-swatch-red" title="Not a Support Email"></span>';
+		}
+
+		if($this->model['auto_reply']){
+			$auto_class = "glyphicon glyphicon-send";
+			$auto_title = "Auto Reply On";
+		}else{
+			$auto_class = "glyphicon glyphicon-send atk-swatch-red";
+			$auto_title = "Auto Reply Off";
+		}
+
+		$auto_send_html = '<span class="'.$auto_class.'" title="'.$auto_title.'"> </span>';
+
+		$str = '<div class="text-center atk-size-giga '.$status.' "> '.$this->model['email_username']." ".$support_html." ".$auto_send_html.'</div>';
+
 		$str .= '<div class="atk-row">';
 
 		$str.= '<div class="atk-col-6">';//OutGoing Email Setting Column1
