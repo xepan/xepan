c10c73bc5810662f744652f34109695d55f10aae
sinharakesh <rksinha.btech@gmail.com>
2015-05-27 17:54:28 +0530

    Email View Devided into small pieces

---
+++ epan-components/xCRM/lib/View/Email.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace xCRM;
+
+class View_Email extends \View{
+
+	function init(){
+		parent::init();
+		
+		$tab = $this->add('Tabs');
+		$associate_tab = $tab->addTab('Associate')->addClass('atk-box');
+		$other_tab = $tab->addTab('Other')->addClass('atk-box');
+		
+		$email = $associate_tab->add('xCRM/Model_AssociatedEmail')->debug();
+		$email->getElement('subject')->caption('Emails');
+		$emails = $email->loadDepartmentEmails();
+		if(!$emails){
+			$emails = $email->addCondition('id',-1);
+		}
+
+		//SHOW ONLY CUSTOMER EMAILS
+		if($_GET['customer_id']){
+			$emails->addCondition(
+				$emails->dsql()->orExpr()
+							->where(
+									$emails->dsql()->andExpr()
+									->where('from','Customer')
+									->where('from_id',$_GET['customer_id'])
+								)
+							->where(
+									$emails->dsql()->andExpr()
+										->where('to','Customer')
+										->where('to_id',$_GET['customer_id'])
+								)
+					);
+
+		}
+
+		if($_GET['supplier_id']){
+
+			$emails->addCondition(
+				$emails->dsql()->orExpr()
+							->where(
+									$emails->dsql()->andExpr()
+									->where('from','Supplier')
+									->where('from_id',$_GET['supplier_id'])
+								)
+							->where(
+									$emails->dsql()->andExpr()
+										->where('to','Supplier')
+										->where('to_id',$_GET['supplier_id'])
+								)
+					);			
+		}
+
+		if($_GET['affiliate_id']){
+
+			$emails->addCondition(
+				$emails->dsql()->orExpr()
+							->where(
+									$emails->dsql()->andExpr()
+									->where('from','Affiliate')
+									->where('from_id',$_GET['affiliate_id'])
+								)
+							->where(
+									$emails->dsql()->andExpr()
+										->where('to','Affiliate')
+										->where('to_id',$_GET['affiliate_id'])
+								)
+					);			
+		}
+
+		$mail_crud=$associate_tab->add('CRUD',array('grid_class'=>'xCRM/Grid_Email'));
+		$mail_crud->setModel($emails,array(),array('subject','to_email','from_email','message','from','id','from_id','direction','task_id','task_status','from_name','cc','bcc'));
+		$mail_crud->add('xHR/Controller_Acl');
+
+
+		$other_mail_crud = $other_tab->add('CRUD',array('grid_class'=>'xCRM/Grid_Email'));
+		$other_mail_crud->setModel($this->add('xCRM/Model_OtherEmail'),array(),array('subject','to_email','from_email','message','from','id','from_id','direction','task_id','task_status','from_name','cc','bcc'));
+		$other_mail_crud->add('xHR/Controller_Acl');
+
+	}
+}
--- epan-components/xHR/page/owner/xmail.php
+++ epan-components/xHR/page/owner/xmail.php
@@ -26,69 +26,8 @@ class page_xHR_page_owner_xmail extends page_xHR_page_owner_main{
 		$left_col->add('xCRM/View_MemberForEmail',array('model'=>$this->add('xShop/Model_AffiliateForEmail'),'member_type'=>'Affiliate'))->addStyle('margin-top','5%');
 
 //Emails--------------------------------------------------------------------------------------
-		$email = $this->add('xCRM/Model_Email');
-		$email->getElement('subject')->caption('Emails');
-		$emails = $email->loadDepartmentEmails();
-		if(!$emails){
-			$emails = $email->addCondition('id',-1);
-		}
-
-		//SHOW ONLY CUSTOMER EMAILS
-		if($_GET['customer_id']){
-			$emails->addCondition(
-				$emails->dsql()->orExpr()
-							->where(
-									$emails->dsql()->andExpr()
-									->where('from','Customer')
-									->where('from_id',$_GET['customer_id'])
-								)
-							->where(
-									$emails->dsql()->andExpr()
-										->where('to','Customer')
-										->where('to_id',$_GET['customer_id'])
-								)
-					);
-
-		}
-
-		if($_GET['supplier_id']){
-
-			$emails->addCondition(
-				$emails->dsql()->orExpr()
-							->where(
-									$emails->dsql()->andExpr()
-									->where('from','Supplier')
-									->where('from_id',$_GET['supplier_id'])
-								)
-							->where(
-									$emails->dsql()->andExpr()
-										->where('to','Supplier')
-										->where('to_id',$_GET['supplier_id'])
-								)
-					);			
-		}
-
-		if($_GET['affiliate_id']){
-
-			$emails->addCondition(
-				$emails->dsql()->orExpr()
-							->where(
-									$emails->dsql()->andExpr()
-									->where('from','Affiliate')
-									->where('from_id',$_GET['affiliate_id'])
-								)
-							->where(
-									$emails->dsql()->andExpr()
-										->where('to','Affiliate')
-										->where('to_id',$_GET['affiliate_id'])
-								)
-					);			
-		}
-
-		$mail_crud=$right_col->add('CRUD',array('grid_class'=>'xCRM/Grid_Email'));
-		$mail_crud->setModel($emails,array(),array('subject','to_email','from_email','message','from','id','from_id','direction','task_id','task_status','from_name','cc','bcc'));
-	
-		$mail_crud->add('xHR/Controller_Acl');
+		$right_col->add('xCRM/View_Email');
+		
 		// $this->js(true)->_selector('*')->xtooltip();
 	}
 
