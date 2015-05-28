05cb276a6664c9853124db0f15d1718fb08ca417
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-28 13:31:02 +0530

    Bounce Handler Added

---
+++ page/cron/bouncecheck.php
@@ -0,0 +1,98 @@
+<?php
+
+class page_cron_bouncecheck extends Page {
+	
+	function init(){
+		parent::init();
+
+
+		$cwsMailBounceHandler = new CwsMailBounceHandler();
+		$cwsMailBounceHandler->test_mode = true; // default false
+		// $cwsMailBounceHandler->debug_verbose = CWSMBH_VERBOSE_DEBUG; // default CWSMBH_VERBOSE_QUIET
+		$cwsMailBounceHandler->purge = true; // default false
+		//$cwsMailBounceHandler->disable_delete = false; // default false
+		//$cwsMailBounceHandler->open_mode = CWSMBH_OPEN_MODE_IMAP; // default CWSMBH_OPEN_MODE_IMAP
+		//$cwsMailBounceHandler->move_soft = false; // default false
+		//$cwsMailBounceHandler->folder_soft = 'INBOX.soft'; // default 'INBOX.hard' - NOTE: for open_mode IMAP it must start with 'INBOX.'
+		//$cwsMailBounceHandler->move_hard = false; // default false
+		//$cwsMailBounceHandler->folder_hard = 'INBOX.hard'; // default 'INBOX.soft' - NOTE: for open_mode IMAP it must start with 'INBOX.'
+		/**
+		* .eml folder
+		*/
+		//$cwsMailBounceHandler->open_mode = CWSMBH_OPEN_MODE_FILE;
+		//if ($cwsMailBounceHandler->openFolder('emls/')) {
+		//$cwsMailBounceHandler->processMails();
+		//}
+		/**
+		* .eml file
+		*/
+		//$cwsMailBounceHandler->open_mode = CWSMBH_OPEN_MODE_FILE;
+		//if ($cwsMailBounceHandler->openFile('test/01.eml')) {
+		// $cwsMailBounceHandler->processMails();
+		//}
+		/**
+		* Local mailbox
+		*/
+		//$cwsMailBounceHandler->open_mode = CWSMBH_OPEN_MODE_IMAP;
+		//if ($cwsMailBounceHandler->openImapLocal('/home/email/temp/mailbox')) {
+		// $cwsMailBounceHandler->processMails();
+		//}
+		/**
+		* Remote mailbox
+		*/
+		$cwsMailBounceHandler->open_mode = CWSMBH_OPEN_MODE_IMAP;
+		$cwsMailBounceHandler->host = 'sun.rightdns.com'; // Mail host server ; default 'localhost'
+		$cwsMailBounceHandler->username = 'bounce@xavoc.com'; // Mailbox username
+		$cwsMailBounceHandler->password = ''; // Mailbox password
+		$cwsMailBounceHandler->port = 993; // the port to access your mailbox ; default 143, other common choices are 110 (pop3), 995 (gmail)
+		//$cwsMailBounceHandler->service = 'imap'; // the service to use (imap or pop3) ; default 'imap'
+		$cwsMailBounceHandler->service_option = 'ssl'; // the service options (none, tls, notls, ssl) ; default 'notls'
+		//$cwsMailBounceHandler->cert = CWSMBH_CERT_NOVALIDATE; // certificates validation (CWSMBH_CERT_VALIDATE or CWSMBH_CERT_NOVALIDATE) if service_option is 'tls' or 'ssl' ; default CWSMBH_CERT_NOVALIDATE
+		//$cwsMailBounceHandler->boxname = 'TEST'; // the mailbox to access ; default 'INBOX'
+		if ($cwsMailBounceHandler->openImapRemote()) {
+			$cwsMailBounceHandler->processMails();
+		}
+		echo "<pre>";
+		print_r($cwsMailBounceHandler->result);
+		echo "</pre>";
+		$result = $cwsMailBounceHandler->result;
+		
+		if($result['counter']['processed']){
+			foreach ($result['msgs'] as $msg) {
+				if($msg['type']=='bounce'){
+					foreach ($msg['recipients'] as $receipent) {
+						if($receipent['remove']){
+							$this->removeEmail($receipent['email']);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	function removeEmail($email){
+		echo "removing $email from customers<br/>";
+		echo "removing $email from suppliers<br/>";
+		echo "removing $email from affiliates<br/>";
+		echo "removing $email from employees<br/>";
+		echo "removing $email from subscribers<br/>";
+
+		$this->add('xEnquiryNSubscription/Model_Subscription')
+			->addCondition('email','like',"%$email%")
+			->each(function($obj)use($email){
+				$emails_count=explode(",", $obj['email']);
+				if(count($emails_count)>1){
+					$obj['remark'] = $obj['remark']. "<br>\n\r ". $email. " bounced and removed. ";
+					if(($key = array_search($email, $emails_count)) !== false) {
+					    unset($emails_count[$key]);
+					}
+					$obj['email'] = implode(", ", $emails_count);
+					$obj->save();
+				}else{
+					$obj['is_ok']=false;
+					$obj->save();
+				}
+			});
+	}
+
+}
