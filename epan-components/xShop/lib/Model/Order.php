ec8c380cbcfab41cd0b9d38eb0e801dedf6207cd
sinharakesh <rksinha.btech@gmail.com>
2015-05-29 14:31:40 +0530

    setorder desc

--- epan-components/xShop/lib/Model/Order.php
+++ epan-components/xShop/lib/Model/Order.php
@@ -80,7 +80,7 @@ class Model_Order extends \Model_Document{
 		
 		$this->addHook('afterSave',$this);
 		$this->addHook('beforeDelete',$this);
-
+		$this->setOrder('id','desc');
 		// $this->add('dynamic_model/Controller_AutoCreator');
 	}
 
