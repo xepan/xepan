d35cee985eaa1b7a4e57587f10de9090035c589d
sinharakesh <rksinha.btech@gmail.com>
2015-05-30 14:12:34 +0530

    print quotation corrected

--- epan-components/xShop/page/owner/printquotation.php
+++ epan-components/xShop/page/owner/printquotation.php
@@ -12,6 +12,11 @@ class page_xShop_page_owner_printquotation extends Page{
 
 		if(!$quotation->loaded()) $this->add('View_Warning')->set('Quotation Not Found');
 
+		$tnc=$quotation->termAndCondition();
+		$print_order = $quotation->add('xShop/View_QuotationDetail',array('show_department'=>false,'show_price'=>true,'show_customfield'=>true));
+		$print_order->setModel($quotation->itemrows());
+		$quotation_detail_html = $print_order->getHTML(false);
+
 		$customer = $quotation->customer();
 		$customer_email=$customer->get('customer_email');
 
@@ -29,6 +34,8 @@ class page_xShop_page_owner_printquotation extends Page{
 		$email_body = str_replace("{{customer_email}}", $customer['customer_email'], $email_body);
 		$email_body = str_replace("{{quotation_no}}", $quotation['name'], $email_body);
 		$email_body = str_replace("{{quotation_date}}", $quotation['created_at'], $email_body);
+		$email_body = str_replace("{{quotation_detail}}", $quotation_detail_html, $email_body);
+		$email_body = str_replace("{{terms_and_conditions}}", $tnc['terms_and_condition']?$tnc['terms_and_condition']:" ", $email_body);
 		//END OF REPLACING VALUE INTO ORDER DETAIL EMAIL BODY
 		
 		echo $email_body;
