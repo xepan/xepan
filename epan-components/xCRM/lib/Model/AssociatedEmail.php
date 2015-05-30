a783059f3e6fa59db81909875d346835fa45475c
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-05-31 00:00:27 +0530

    Show ermaployees email too if related to department

--- epan-components/xCRM/lib/Model/AssociatedEmail.php
+++ epan-components/xCRM/lib/Model/AssociatedEmail.php
@@ -7,8 +7,8 @@ class Model_AssociatedEmail extends Model_Email{
 		parent::init();
 
 		$this->addCondition($this->_dsql()->orExpr()
-							->where('from',array('Customer','Supplier','Affiliate'))
-							->where('to',array('Customer','Supplier','Affiliate'))
+							->where('from',array('Customer','Supplier','Affiliate','Employee'))
+							->where('to',array('Customer','Supplier','Affiliate','Employee'))
 							);
 
 		
