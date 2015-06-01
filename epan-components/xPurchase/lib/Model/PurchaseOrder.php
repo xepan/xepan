3fe5ee89aa7d9f1456d767358bba8153941f916e
sinharakesh <rksinha.btech@gmail.com>
2015-06-01 20:30:49 +0530

    after save hook pe round amount added

--- epan-components/xPurchase/lib/Model/PurchaseOrder.php
+++ epan-components/xPurchase/lib/Model/PurchaseOrder.php
@@ -28,10 +28,19 @@ class Model_PurchaseOrder extends \Model_Document{
 		$this->hasMany('xPurchase/PurchaseOrderAttachment','related_document_id',null,'Attachements');
 
 		$this->addHook('beforeDelete',$this);
+		$this->addHook('afterSave',$this);
 
 		$this->addExpression('orderitem_count')->set($this->refSQL('xPurchase/PurchaseOrderItem')->count());
 		// $this->add('dynamic_model/Controller_AutoCreator');
 		$this->setOrder('id','desc');
+	}
+
+	function afterSave(){
+		$shop_config = $this->add('xShop/Model_Configuration')->tryLoadAny();
+		if($shop_config['is_round_amount_calculation']){
+			$this['net_amount'] = round($this['net_amount'],0);
+			$this->save();
+		}
 	}
 
 	function beforeDelete(){
