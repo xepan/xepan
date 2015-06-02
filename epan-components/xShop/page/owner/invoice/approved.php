a3961c2a8969ad7383a895006bd0684e8c75fd43
xavocvijay <guddavijay@gmail.com>
2015-06-02 17:15:09 +0530

    Remove Unknow Column on Invoice pages

--- epan-components/xShop/page/owner/invoice/approved.php
+++ epan-components/xShop/page/owner/invoice/approved.php
@@ -7,7 +7,7 @@ class page_xShop_page_owner_invoice_approved extends page_xShop_page_owner_main{
 		$invoice_draft = $this->add('xShop/Model_Invoice_Approved');
 		
 		$crud=$this->add('CRUD',array('grid_class'=>'xShop/Grid_Invoice'));
-		$crud->setModel($invoice_draft,array('customer_id','termsandcondition_id','discount','billing_address','invoiceitem_count'),array('name','customer','customer_id','invoice_no','sales_order','total_amount','tax','gross_amount','discount','net_amount','invoiceitem_count'));
+		$crud->setModel($invoice_draft,array('customer_id','termsandcondition_id','discount','billing_address','invoiceitem_count'),array('name','customer','customer_id','sales_order','total_amount','tax','gross_amount','discount','net_amount','invoiceitem_count'));
 
 		// if(!$crud->isEditing()){
 		// 	$crud->grid->fooHideBoth('discount');
--- epan-components/xShop/page/owner/invoice/redesign.php
+++ epan-components/xShop/page/owner/invoice/redesign.php
@@ -4,7 +4,7 @@ class page_xShop_page_owner_invoice_redesign extends page_xShop_page_owner_main{
 		parent::init();
 
 		$crud=$this->add('CRUD',array('grid_class'=>'xShop/Grid_Invoice'));
-		$crud->setModel('xShop/Model_Invoice_Redesign',array('customer_id','termsandcondition_id','discount','billing_address','invoiceitem_count'),array('name','customer_id','customer','invoice_no','sales_order','total_amount','tax','gross_amount','discount','net_amount','invoiceitem_count'));
+		$crud->setModel('xShop/Model_Invoice_Redesign',array('customer_id','termsandcondition_id','discount','billing_address','invoiceitem_count'),array('name','customer_id','customer','sales_order','total_amount','tax','gross_amount','discount','net_amount','invoiceitem_count'));
 		$crud->add('xHR/Controller_Acl');
 			
 	}
