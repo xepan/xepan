651f9e3c2ce7401b70f6cf86f7d62de65bb7f422
xavocvijay <guddavijay@gmail.com>
2015-06-03 16:29:55 +0530

    activity action added

--- epan-components/xShop/lib/Model/Quotation/Processing.php
+++ epan-components/xShop/lib/Model/Quotation/Processing.php
@@ -3,7 +3,13 @@
 namespace xShop;
 
 class Model_Quotation_Processing extends Model_Quotation{
-
+	public $actions=array(
+			'can_view'=>array(),
+			'allow_edit'=>array(),
+			'allow_del'=>array(),
+			'can_send_via_email'=>array(),
+			'can_see_activities'=>array(),
+		);
 	function init(){
 		parent::init();
 
