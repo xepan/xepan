519f84945dd06de7f1ac246af65b6cc3a475819b
xavocvijay <guddavijay@gmail.com>
2015-06-03 16:26:24 +0530

    'can_see_activities'=>array(), added

--- epan-components/xShop/lib/Model/Invoice/Completed.php
+++ epan-components/xShop/lib/Model/Invoice/Completed.php
@@ -6,7 +6,8 @@ class Model_Invoice_Completed extends Model_SalesInvoice{
 	public $actions=array(
 			'can_view'=>array(),
 			'can_cancel'=>array(),
-			'can_send_via_email'=>array()
+			'can_send_via_email'=>array(),
+			'can_see_activities'=>array(),
 		);
 
 	function init(){
