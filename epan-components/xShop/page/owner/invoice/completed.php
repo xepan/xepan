5e579e31c3f23e872b93028a8a2a9ed7309582a5
sinharakesh <rksinha.btech@gmail.com>
2015-06-07 13:08:55 +0530

    Order amount Section beautify

--- epan-components/xShop/page/owner/invoice/completed.php
+++ epan-components/xShop/page/owner/invoice/completed.php
@@ -7,5 +7,6 @@ class page_xShop_page_owner_invoice_completed extends page_xShop_page_owner_main
 		$crud->setModel('xShop/Model_Invoice_Completed',array('customer_id','termsandcondition_id','discount','shipping_charge','billing_address','invoiceitem_count','narration','created_at'),array('name','customer_id','customer','sales_order','total_amount','tax','gross_amount','discount','net_amount','invoiceitem_count','shipping_charge','created_at','updated_at','updated_date','created_date'));
 		$crud->add('xHR/Controller_Acl');
 		
+		$crud->grid->addPaginator($ipp=100);	
 	}
 }		
--- epan-components/xShop/page/owner/invoice/redesign.php
+++ epan-components/xShop/page/owner/invoice/redesign.php
@@ -6,6 +6,6 @@ class page_xShop_page_owner_invoice_redesign extends page_xShop_page_owner_main{
 		$crud=$this->add('CRUD',array('grid_class'=>'xShop/Grid_Invoice'));
 		$crud->setModel('xShop/Model_Invoice_Redesign',array('customer_id','termsandcondition_id','discount','shipping_charge','billing_address','invoiceitem_count','narration'),array('name','customer_id','customer','sales_order','total_amount','tax','gross_amount','discount','net_amount','invoiceitem_count','shipping_charge','created_at','updated_at','updated_date','created_date'));
 		$crud->add('xHR/Controller_Acl');
-			
+		$crud->grid->addPaginator($ipp=100);
 	}
 }		
