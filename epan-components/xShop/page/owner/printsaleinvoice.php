09790eae5010fdd64f95251af965c439022901bf
sinharakesh <rksinha.btech@gmail.com>
2015-06-09 13:08:00 +0530

    Print only element with are required

--- epan-components/xShop/page/owner/printsaleinvoice.php
+++ epan-components/xShop/page/owner/printsaleinvoice.php
@@ -5,6 +5,19 @@ class page_xShop_page_owner_printsaleinvoice extends Page{
 	function init(){
 		parent::init();
 
+		
+		$css=array(
+			'templates/css/noprint.css',
+		);
+		
+		foreach ($css as $css_file) {
+			$link = $this->add('View')->setElement('link');
+			$link->setAttr('rel',"stylesheet");
+			$link->setAttr('type',"text/css");
+			$link->setAttr('href',$css_file);
+			$link->setAttr('media','print');
+		}
+
 		$invoice_id = $this->api->StickyGET('saleinvoice_id');
 		if(!$invoice_id) $this->add('View_Warning')->set('Invoice Not Found');
 
@@ -12,7 +25,7 @@ class page_xShop_page_owner_printsaleinvoice extends Page{
 
 		if(!$invoice->loaded()) $this->add('View_Warning')->set('Invoice Not Found');
 
-		
+		echo $link->getHtml();
 		echo $invoice->parseEmailBody();
 		exit;
 	}
