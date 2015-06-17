1b91e9d59f2574973774e4bb0dd757e7ea8cebab
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-06-17 11:41:54 +0530

    Working on report page

---
+++ epan-components/xShop/page/owner/report.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Page class
+ */
+class page_xShop_page_owner_report extends page_xShop_page_owner_main{
+    public ='';
+
+    /**
+     * Initialize the page
+     *
+     * @return void
+     */
+	function init()
+    {
+		parent::init();
+		
+	}
+}
