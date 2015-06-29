16e1071f7faa48866197255645a08520e81a43d5
sinharakesh <rksinha.btech@gmail.com>
2015-06-30 01:29:24 +0530

    ImageLibraryCategory add

---
+++ epan-components/xShop/lib/Model/ImageLibraryCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace xShop;
+
+class Model_ImageLibraryCategory extends \Model_Table {
+	var $table= "xshop_image_library_category";
+	function init(){
+		parent::init();
+		
+		$this->hasOne('Epan','epan_id');
+		$this->addCondition('epan_id',$this->api->current_website->id);
+
+		$f = $this->addField('name')->caption('Category Name');
+		
+		//$this->add('dynamic_model/Controller_AutoCreator');		
+	}
+}
