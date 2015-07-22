86244f8938d6978b139e4d56989a03ddfc733a24
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-07-22 12:20:28 +0530

    New Documentation Application Added

---
+++ epan-components/xDocApp/page/owner/update.php
@@ -0,0 +1,18 @@
+<?php
+
+class page_xDocApp_page_owner_update extends page_componentBase_page_update {
+		
+	public $git_path="https://github.com/xepan/xDocApp.git"; // Put your components git path here
+
+	function init(){
+		parent::init();
+
+		// 
+		// Code To run before update
+		
+		$this->update($dynamic_model_update=true, $git_update=false);
+		$this->add('View_Info')->set('Component Updated Successfully');
+		
+		// Code to run after update
+	}
+}
