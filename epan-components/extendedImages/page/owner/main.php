4bd30647f302af183f4b5a2d62cf15e7c509f7f3
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-08-26 17:15:52 +0530

    Error bug Fixed

--- epan-components/extendedImages/page/owner/main.php
+++ epan-components/extendedImages/page/owner/main.php
@@ -2,7 +2,7 @@
 
 class page_extendedImages_page_owner_main extends page_componentBase_page_owner_main {
 	function initMainPage(){
-		$this->h1->setHTML('<i class="fa fa-picture-o"></i> '.$this->component_name.'<small> Images with magics</small>');
+		$this->add('H1')->setHTML('<i class="fa fa-picture-o"></i> '.$this->component_name.'<small> Images with magics</small>');
 	}
 
 
