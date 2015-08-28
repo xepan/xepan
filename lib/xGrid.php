341fbfda2d91b14a8eff17b329a8a9efff6daf00
sinharakesh <rksinha.btech@gmail.com>
2015-08-28 16:08:17 +0530

    xGrid NameSpace and Pathfinder add

--- lib/xGrid.php
+++ lib/xGrid.php
@@ -2,12 +2,23 @@
 
 class xGrid extends Grid{
 	public $grid_template = 'grid';
-
+	public $grid_template_path = "";//epan-components/
 	function precacheTemplate(){
 
 	}
 
 	function defaultTemplate(){
+		if($this->grid_template_path){
+			$this->app->pathfinder->base_location->addRelativeLocation(
+			    $this->grid_template_path, array(
+			        'php'=>'lib',
+			        'template'=>'templates',
+			        'css'=>array('templates/css','templates/js'),
+			        'img'=>array('templates/css','templates/js'),
+			        'js'=>'templates/js',
+			    )
+			);
+		}
 		return array($this->grid_template);
 	}	
 }
