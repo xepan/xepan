2134c7c092961378e4af9f44a784b1e2c4694493
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-09-04 20:47:22 +0530

    Generic Search doc page

--- page/owner/searchdoc.php
+++ page/owner/searchdoc.php
@@ -4,24 +4,66 @@ class page_owner_searchdoc extends page_base_owner{
 	
 	function page_index(){
 
-		$domain = array(
-			// 'reference' => array(
-			// 		'model'=>'Model_Name',
-			// 		'view'=>'View_Name',
-			// 		'search_phrases'=>array('field1','field2',...)
-			// 	)
+		$document = "Any || xShop/Customer";
+		$term = "";
 
-			);
+		// sensitize term like add + befrore order, customer etc
 
-		$data = $this->add('xShop/Model_Customer');
-		$data->addCondition('customer_name','like','%'.$_GET['term'].'%');
+		$structure_array =[
+			'xShop/Customer'=>[
+				'search_field'=>'search_string',
+				'return_fields'=>[
+					'name'=>'name',
+					'by' =>'customer/supplier/employee', /*will get name in by of producer of document*/
+					'to' =>'customer/supplier/employee', /*will get name in by of producer of document*/
+					'date' =>'created_at', /*will get name in by of producer of document*/
+					'status' =>'status', /*will get name in by of producer of document*/
+					'document_model' =>'xShop/Customer', /*will get name in by of producer of document*/
+					'id' =>'id', /*will get name in by of producer of document*/
+				]
+			],
+			'DocumentName2'=>[
+				'search_field'=>'search_string',
+				'return_fields'=>[
+					'name'=>'name',
+					'by' =>'customer/supplier/employee', /*will get name in by of producer of document*/
+					'to' =>'customer/supplier/employee', /*will get name in by of producer of document*/
+					'date' =>'created_at', /*will get name in by of producer of document*/
+					'status' =>'status', /*will get name in by of producer of document*/
+					'document_model' =>'xShop/Customer', /*will get name in by of producer of document*/
+					'id' =>'id', /*will get name in by of producer of document*/
+				]
+			]
+		];
 
-		$data= $data->getRows(array('id', 'name'));
+		$data=[];
+		foreach ($structure_array as $model=>$structure) {
+			if($document!='Any' && $document !=$model) continue;
+				$m=$this->add($model);
+				$m->addExpression('relevance')->set('MATCH('.$structure['search_field'].') AGAINST ("'.$term.'") IN NATURAL LANGUAGE');
+				$m->setOrder($m->dsql()->expr('[0]',$m->getElement('relevance')),'DESC');
+				if($document =='Any')
+					$m->setLimit(5);
+				else
+					$m->setLimit(15);
 
-		foreach ($data as &$row) {
-            $row['id'] = (string)$row['id'];
-            $row['key'] = (string)$row['id'];
-        }
+				$m->getRows();
+				$m_data=[];
+				foreach ($m as $tm) {
+					$m_data['relevance'] = $m['relevance'];
+					foreach ($structure['return_fields'] as $standard => $model_field) {
+						$m_data[$standard] = $m[$model_field];
+					}
+				}
+
+				$data[] = $m_data;
+		}
+
+		// Sort this $data array by its all relevance
+		// take first 15 records
+		// json echo ..
+		// baat khatam
+
         echo json_encode($data);
         exit;
 
