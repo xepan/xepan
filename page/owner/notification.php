2b82c9599f76eb03ee61ee8bda3eb2c4be9a8283
sinharakesh <rksinha.btech@gmail.com>
2015-09-17 15:02:58 +0530

    notification temporary off

--- page/owner/notification.php
+++ page/owner/notification.php
@@ -6,7 +6,7 @@ class page_owner_notification extends page_base_owner{
 
 			set_time_limit(30);
 			sleep(1);
-			$ns=$this->add('Controller_NotificationSystem');
+			// $ns=$this->add('Controller_NotificationSystem');
 			// while(true){
 				$ns->getNotification();
 			// 	usleep(5000);
