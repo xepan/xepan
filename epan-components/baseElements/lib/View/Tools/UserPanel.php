1fbce1b547471b9f78fa30fcb74d10fc7836b162
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-11-07 16:25:35 +0530

    Crop Image Hight fixed & User panel Option Added Hide User-name Or Show

--- epan-components/baseElements/lib/View/Tools/UserPanel.php
+++ epan-components/baseElements/lib/View/Tools/UserPanel.php
@@ -59,7 +59,6 @@ class View_Tools_UserPanel extends \componentBase\View_Component{
 					$sign_up_field = $bottom_view->add('View')->setHTML($user_panel_btn_registration_name)->addClass('xepan-login-registration-link');
 					$sign_up_field->js('click',$this->js()->reload(array('user_selected_form'=>'new_registration')));
 				}
-				
 				if($this->html_attributes['show_forgot_password'] and $display !== 'forget_password'){
 					$forgot_field = $bottom_view->add('View')->setHTML($user_panel_forgot_pass)->addClass('xepan-login-forget-password-link');
 					$forgot_field->js('click',$this->js()->reload(array('user_selected_form'=>'forget_password')));
@@ -77,18 +76,26 @@ class View_Tools_UserPanel extends \componentBase\View_Component{
 				$this->api->redirect($this->api->url(null,array('subpage'=>$this->html_attributes['user_panel_after_login_page'])));
 			}
 
+
+
+
 			// create hello user View
 			if($this->html_attributes['user_panel_show_logout_view']){
 				$str = "";
 				$url = $this->html_attributes['user_panel_after_logout_page'];
-				$logout_prefix = isset($this->html_attributes['user_panel_logout_prefix'])?$this->html_attributes['user_panel_logout_prefix']:"Hello ";
+				//display user name or not
+				$view="";
+				if(!$this->html_attributes['show_name_or_username']){
+					$logout_prefix = isset($this->html_attributes['user_panel_logout_prefix'])?$this->html_attributes['user_panel_logout_prefix']:"Hello ";
+					$view=$this->add('View')->setHTML($logout_prefix.$this->api->auth->model['username'].$str);
+				}
 				$logout_caption = isset($this->html_attributes['user_panel_logout_caption'])?$this->html_attributes['user_panel_logout_caption']:"Logout ";
 				if(strpos($url, "http://") !== false){
 					$str .= '<a class="xepan-user-logout-bth" href="index.php?page=epanlogout&logout_url='.$this->html_attributes['user_panel_after_logout_page'].'">'.$logout_caption.'</a>';
 				}else
 					$str .= '<a class="xepan-user-logout-bth" href="index.php?page=logout"> '.$logout_caption.'</a>';
 
-				$this->add('View')->setHTML($logout_prefix.$this->api->auth->model['username'].$str);
+				$this->add('View')->setHTML($str);
 			}
 
 		}
