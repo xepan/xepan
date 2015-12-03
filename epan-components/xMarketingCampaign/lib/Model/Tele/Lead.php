876c74b21cb33f8271521a013b653fb07cdaf304
sinharakesh <rksinha.btech@gmail.com>
2015-12-03 13:04:32 +0530

    association function that associte lead with campaign

--- epan-components/xMarketingCampaign/lib/Model/Tele/Lead.php
+++ epan-components/xMarketingCampaign/lib/Model/Tele/Lead.php
@@ -19,16 +19,20 @@ class Model_Tele_Lead extends Model_Lead {
 
 	function init(){
 		parent::init();
-
-		// $this->hasOne('Epan','epan_id');
-		// $this->addCondition('epan_id',$this->api->current_website->id);
-		
-		$this->hasOne('xMarketingCampaign/Tele_Campaign','telecampaign_id');
-
-		// $this->addField('name')->sortable(true);
+		// $this->hasOne('xMarketingCampaign/Tele_Campaign','telecampaign_id');
 		$this->hasMany('xMarketingCampaign/Tele_CampLeadAsso','telelead_id');
-		
+			
 		$this->add('dynamic_model/Controller_AutoCreator');
+	}
+
+	function association($telecampaign_id){
+		if(!$this->loaded())
+			throw new \Exception('For association model must be loaded');
+		
+		$asso = $this->add('xMarketingCampaign/Model_Tele_CampLeadAsso');
+		$asso['telecampaign_id'] = $telecampaign_id;
+		$asso['telelead_id'] = $this->id;
+		$asso->save();
 	}
 	
 }
