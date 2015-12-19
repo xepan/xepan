1437b8712bf3152fd446ce648ec6919d7f63dbdc
vijay mali <guddavijay@gmail.com>
2015-12-19 16:04:25 +0530

    member profile view updated

--- epan-components/xShop/lib/View/Tools/MemberAccount.php
+++ epan-components/xShop/lib/View/Tools/MemberAccount.php
@@ -13,9 +13,25 @@ class View_Tools_MemberAccount extends \componentBase\View_Component{
 		}
 		
 		$this->template->trySet('customer',$this->api->auth->model['name']);
-		// $this->template->trySet('user_name',$this->api->auth->model['user_name']);
+		$user=$this->add('Model_Users')->load($this->api->auth->model->id);
+		$created_at=$user->get('created_at');
+		$last_login=$user->get('last_login_date');
+		if($user['type']==50){
+				$user['type']='FrontEndUser';
+		}elseif ($user['type']==100) {
+					$user['type']='SuperUser';
+		}else{
+			$user['type']='BackEndUser';
 
-			// $this->add('H1')->set($this->api->auth->model['name'])->addClass('xsnb-member-name');
+		}
+		$user_type=$user->get('type');
+		$user_email=$user->get('email');
+		$this->template->trySet('created_at',date('M-d',strtotime($created_at)));
+		$this->template->trySet('last_login_date',date('M-d',strtotime($last_login)));
+		$this->template->trySet('user_type',$user_type);
+		$this->template->trySet('email',$user_email);
+
+		// $this->add('H1')->set($this->api->auth->model['name'])->addClass('xsnb-member-name');
 			
 			$tab = $this->add('Tabs')->addClass('nav-stacked');
 			//Account Information
