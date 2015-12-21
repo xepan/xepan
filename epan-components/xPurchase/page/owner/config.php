32750363039e567ce818b277708f386ae500bb86
vijay mali <guddavijay@gmail.com>
2015-12-21 17:09:57 +0530

    Purchase Configuration extra code removed

--- epan-components/xPurchase/page/owner/config.php
+++ epan-components/xPurchase/page/owner/config.php
@@ -1,42 +1,23 @@
 <?php
 class page_xPurchase_page_owner_config extends page_xPurchase_page_owner_main{
 	function page_index(){
+		$this->app->title=$this->api->current_department['name'] .': Configuration';
+		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-users"></i> Purchase Department Management <small> Manage your shops here</small>');
 		$crud= $this->add('CRUD',array('allow_add'=>false,'allow_del'=>false));//,array('grid_class'=>'xShop/Grid_Shop'));
 		$crud->setModel('xShop/Shop',array('name'));
 		$crud->grid->addColumn('expander','configuration',array("descr"=>"Configuration",'icon'=>'cog'));
 
 	}
 	function page_configuration(){
-		$application_id = $this->api->StickyGET('xshop_application_id');
-		
-		$config_model=$this->add('xShop/Model_Configuration')->addCondition('application_id',$application_id)->tryLoadAny();
-		
-		$tab=$this->add('Tabs');
-			// $comment_tab=$tab->addTab('Comments');
 
-			// 	$comment_form=$comment_tab->add('Form_Stacked');
-			// 	$comment_form->setModel($config_model,array('disqus_code'));
-			// 	$comment_form->addSubmit('Update');
-			// 	if($comment_form->Submitted()){
-			// 		$comment_form->Update();
-			// 		$comment_form->js(null,$comment_form->js()->reload())->univ()->successMessage('Update Information')->execute();
-			// 	}
-			// 	$comment_form->addClass('panel panel-default');
-			// 	$comment_form->addStyle('padding','20px');
+		$application_id = $this->api->StickyGET('xshop_application_id');
+		$config_model=$this->add('xShop/Model_Configuration')->addCondition('application_id',$application_id)->tryLoadAny();
+
+		$tab=$this->add('Tabs');
 			
 			$mail_tab=$tab->addTab('Email Templates Layouts');
 			
 				$lay_tab=$mail_tab->add('Tabs');
-				// 	$sales_tab=$lay_tab->addTab('Sales Order');
-				// 		$sales_form=$sales_tab->add('Form_Stacked');
-				// 		$sales_form->setModel($config_model,array('order_detail_email_subject','order_detail_email_body'));
-				// 		$sales_form->addSubmit('Update');
-				// 		if($sales_form->Submitted()){
-				// 			$sales_form->Update();
-				// 			$sales_form->js(null,$sales_form->js()->reload())->univ()->successMessage('Update Information')->execute();
-				// 		}
-				// 		$sales_form->addClass('panel panel-default');
-				// 		$sales_form->addStyle('padding','20px');
 					$purchase_tab=$lay_tab->addTab('Purchase Order');
 						$purchase_form=$purchase_tab->add('Form_Stacked');
 						$purchase_form->setModel($config_model,array('purchase_order_detail_email_subject','purchase_order_detail_email_body'));
@@ -47,28 +28,6 @@ class page_xPurchase_page_owner_config extends page_xPurchase_page_owner_main{
 						}
 						$purchase_form->addClass('panel panel-default');
 						$purchase_form->addStyle('padding','20px');
-					
-					// $quotation_tab=$lay_tab->addTab('Quotation');
-					// 	$q_form=$quotation_tab->add('Form_Stacked');
-					// 	$q_form->setModel($config_model,array('quotation_email_subject','quotation_email_body'));
-					// 	$q_form->addSubmit('Update');
-					// 	if($q_form->Submitted()){
-					// 		$q_form->Update();
-					// 		$q_form->js(null,$q_form->js()->reload())->univ()->successMessage('Update Information')->execute();
-					// 	}
-					// 	$q_form->addClass('panel panel-default');
-					// 	$q_form->addStyle('padding','20px');
-					
-					// $sales_invoice_tab=$lay_tab->addTab('Sales Invoice');
-					// 	$i_form=$sales_invoice_tab->add('Form_Stacked');
-					// 	$i_form->setModel($config_model,array('invoice_email_subject','invoice_email_body'));
-					// 	$i_form->addSubmit('Update');
-					// 	if($i_form->Submitted()){
-					// 		$i_form->Update();
-					// 		$i_form->js(null,$i_form->js()->reload())->univ()->successMessage('Update Information')->execute();
-					// 	}
-					// 	$i_form->addClass('panel panel-default');
-					// 	$i_form->addStyle('padding','20px');
 
 					$purchase_invoice_tab=$lay_tab->addTab('Purchase Invoice');
 						$p_form=$purchase_invoice_tab->add('Form_Stacked');
@@ -81,28 +40,6 @@ class page_xPurchase_page_owner_config extends page_xPurchase_page_owner_main{
 						$p_form->addClass('panel panel-default');
 						$p_form->addStyle('padding','20px');
 
-						// $outsource_tab=$lay_tab->addTab('OutSource Party');
-						// $o_form=$outsource_tab->add('Form_Stacked');
-						// $o_form->setModel($config_model,array('outsource_email_subject','outsource_email_body'));
-						// $o_form->addSubmit('Update');
-						// if($o_form->Submitted()){
-						// 	$o_form->Update();
-						// 	$o_form->js(null,$o_form->js()->reload())->univ()->successMessage('Update Information')->execute();
-						// }
-						// $o_form->addClass('panel panel-default');
-						// $o_form->addStyle('padding','20px');	
-
-					// $cash_voucher_tab=$lay_tab->addTab('Cash Voucher');
-					// 	$cash_form=$cash_voucher_tab->add('Form_Stacked');
-					// 	$cash_form->setModel($config_model,array('cash_voucher_email_subject','cash_voucher_email_body'));
-					// 	$cash_form->addSubmit('Update');
-					// 	if($cash_form->Submitted()){
-					// 		$cash_form->Update();
-					// 		$cash_form->js(null,$cash_form->js()->reload())->univ()->successMessage('Update Information')->execute();
-					// 	}
-					// 	$cash_form->addClass('panel panel-default');
-					// 	$cash_form->addStyle('padding','20px');	
-
 			$number_tab=$tab->addTab('Starting Number');
 			$number_form = $number_tab->add('Form_Stacked');
 			$number_form->setModel($config_model,array('purchase_order_starting_number','purchase_invoice_starting_number'));
@@ -113,7 +50,6 @@ class page_xPurchase_page_owner_config extends page_xPurchase_page_owner_main{
 						}
 			$number_form->addClass('panel panel-default');
 			$number_form->addStyle('padding','20px');
-
 
 			$misc_tab=$tab->addTab('Misc Setting');
 			$misc_form = $misc_tab->add('Form_Stacked');
